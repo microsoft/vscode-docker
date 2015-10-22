74732db8e007013c12c889519bf6d626ebe18f66
Peter Jausovec <peterj@microsoft.com>
2015-10-22 11:26:16 -0700

    Generate docker file and script for NodeJs project
    Adding code that creates a Dockerfile and script for building a Docker
    image and running a Docker container for existing NodeJS projects.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,80 +1,166 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var path = require('path');
+var process = require('process');
+var exec = require('child_process').exec;
 
 var DockerGenerator = yeoman.generators.Base.extend({
   constructor: function () {
-    yeoman.generators.Base.apply(this, arguments);  
-  }, 
-  
+    yeoman.generators.Base.apply(this, arguments);
+    this.ScriptNameNodeJs = 'dockerTask.sh';
+  },
+
   init: function () {
-    this.log(yosay('Welcome to the ' + chalk.red('Docker') + ' generator!'));
-    this.templatedata = {};
+    this.log(yosay('Welcome to the ' + chalk.red('Docker') + ' generator!' + chalk.green('\nLet\'s add Docker magic to your app!')));
   },
-  
-  initializing: function () {
-  },
-  
-  initForTest: function(artifactType) {
-     this.type = artifactType;
-  },
-  
+
   askFor: function () {
-    var done = this.async(); 
-    
+    var done = this.async();
     var prompts = [{
-      type: 'list',
-      name: 'type',
-      message: 'Which Docker artifact do you want to create?',
-      choices: [
-        {
-          name: 'Docker file (' + chalk.bold('Dockerfile') + ')',
-          value: 'dockerfile'
+      type: 'input',
+      name: 'maintainer',
+      message: 'What\'s your GitHub user name?',
+      default: 'someuser'
+    },
+      {
+        type: 'list',
+        name: 'type',
+        message: 'What language is your project using?',
+        choices: [{
+          name: 'Node.js',
+          value: 'nodejs'
         },
-        {
-          name: 'Compose file (' + chalk.bold('Docker-compose.yml') + ')',
-          value: 'composefile'
+          {
+            name: 'Golang',
+            value: 'golang'
+          }]
+      },
+      {
+        type: 'confirm',
+        name: 'addnodemon',
+        message: 'Do you want to use Nodemon?',
+        when: function (answers) {
+          return answers.type === 'nodejs';
         }
-      ]
-    }];
-    
+      },
+      {
+        type: 'input',
+        name: 'portNumber',
+        message: 'Which port is your app running on?',
+        default: "3000",
+        when: function (answers) {
+          return answers.type === 'nodejs';
+        }
+      },
+      {
+        type: 'input',
+        name: 'imageName',
+        message: 'How do you want to name your image?',
+        default: process.cwd().split(path.sep).pop() + '_image',
+      },
+      {
+        type: 'input',
+        name: 'dockerHostName',
+        message: 'What\'s the name of your docker host machine?',
+        default: 'default',
+      }
+    ];
+
     this.prompt(prompts, function (props) {
-      this.type = props.type;
+      this.projectType = props.type;
+      this.maintainer = props.maintainer;
+      this.addnodemon = props.addnodemon;
+      this.portNumber = props.portNumber;
+      this.imageName = props.imageName;
+      this.dockerHostName = props.dockerHostName;
       done();
     }.bind(this));
   },
 
   writing: function () {
-      this.sourceRoot(path.join(__dirname, './templates'));
-      
-      switch (this.type) {
-        case 'dockerfile':
-          this.copy(this.sourceRoot() + '/_Dockerfile', 'Dockerfile');
-          break; 
-        case 'composefile':
-          break; 
-        default: 
-          // unknown.
-          break; 
+    this.sourceRoot(path.join(__dirname, './templates'));
+
+    switch (this.projectType) {
+      case 'nodejs': {
+        this._handleNodeJs();
+        break;
       }
+      case 'golang': {
+        this._handleGolang();
+        break;
+      }
+      default: 
+        // unknown.
+        break;
+    }
   },
 
-  install: function () {
-    // this.installDependencies();
-  }, 
-  
+  _handleGolang: function () {
+    // Nothing here yet.
+  },
+
+  _handleNodeJs: function () {
+    // Add the Nodemon command if selected.
+    if (this.addnodemon) {
+      this.nodeMonCommand = 'RUN npm install nodemon -g';
+    }
+    else {
+      this.nodeMonCommand = '';
+    }
+
+    this.fs.copyTpl(
+      this.templatePath('_Dockerfile.nodejs'),
+      this.destinationPath('Dockerfile'),
+      {
+        imageName: 'node',
+        maintainer: this.maintainer,
+        nodeMonCommand: this.nodeMonCommand,
+        portNumber: this.portNumber,
+      });
+
+    this.fs.copyTpl(
+      this.templatePath('_dockerTaskNodejs.sh'),
+      this.destinationPath(this.ScriptNameNodeJs),
+      {
+        imageName: this.imageName,
+        portNumber: this.portNumber,
+        dockerHostName: this.dockerHostName
+      });
+  },
+
+  _makeBuildScriptExecutable: function () {
+    var done = this.async();
+    exec('chmod +x ' + this.ScriptNameNodeJs, function (err) {
+      if (err) {
+        this.log.error(err);
+        this.log.error('Error making script executable. Run ' + chalk.bold('chmod +x ' + this.ScriptNameNodeJs) + ' manually.');
+        this.error = true;
+      }
+      done();
+    }.bind(this));
+  },
+
   end: function () {
-    switch (this.type) {
-      case 'dockerfile':  
-        this.log('Dockerfile was added to your project.');
+    if (this.error) {
+      this.log(chalk.red(':( errors occured.'));
+    }
+    
+    switch (this.projectType) {
+      case 'nodejs': {
+        this._makeBuildScriptExecutable();
+        this.log('Your project is now ready to run in a Docker container!');
+        this.log('Run ' + chalk.green(this.ScriptNameNodeJs) + ' to build a Docker image and run your app in a container.');
         break;
-      case 'composefile':
-        this.log('Oops, nothing there yet :(');
+      }
+      default:
+        this.log.error('Not implemented yet.');
         break;
-      default: 
-        break; 
     }
   }
 });
---
+++ generators/app/templates/_dockerTaskNodejs.sh
@@ -0,0 +1,68 @@
+imageName="<%= imageName %>"
+containerPort=<%= portNumber %>
+dockerHostName="<%= dockerHostName %>"
+publicPort=8080
+
+# Kills all running containers of an image and then removes them. 
+cleanAll () {
+	# List all running containers that use $imageName, kill them and then remove them.
+    docker kill $(docker ps -a | awk '{ print $1,$2 }' | grep $imageName | awk '{ print $1}') > /dev/null 2>&1;
+	docker rm $(docker ps -a | awk '{ print $1,$2 }' | grep $imageName | awk '{ print $1}') > /dev/null 2>&1; 
+}
+
+# Builds the Docker image.
+buildImage () {
+    cleanAll
+	
+    echo "Building the image $imageName."
+    docker build -t $imageName .
+}
+
+# Runs the container.
+runContainer () {
+    # Check if container is already running, stop it and run a new one. 
+	docker kill $(docker ps -a | awk '{ print $1,$2 }' | grep $imageName | awk '{ print $1}') > /dev/null 2>&1;
+	
+    # Create a container from the image. 
+    docker run -di -p $publicPort:$containerPort $imageName
+	
+	# Open the site.
+	open "http://$(docker-machine ip $dockerHostName):$publicPort"
+}
+
+# Shows the usage for the script.
+showUsage () {
+    echo "Description:"
+	echo "    Builds and runs a Docker image."
+	echo ""
+	echo "Options:"
+	echo "    build: Builds a Docker image (\'$imageName\')."
+	echo "    run: Runs container based on an existing Docker image (\'$imageName\')." 
+	echo "    buildrun: Builds a Docker image and runs the container."
+	echo "    clean: Remove the image \'$imageName\' and all containers based on that image." 
+	echo ""
+	echo "Example:"
+	echo "    ./build.sh build"
+	echo "" 
+	echo "    This will:"
+	echo "        Build a Docker image named $imageName." 
+}
+
+case "$1" in 
+    "build")
+	       buildImage
+		   ;;
+    "run")
+	       runContainer
+		   ;;
+    "clean")
+	       cleanAll
+		   ;; 
+    "buildrun" | "")
+	       buildImage
+		   runContainer
+		   ;;
+    *) 
+	       showUsage
+	       ;;
+esac	   
