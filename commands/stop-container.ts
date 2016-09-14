24029ce08c855cf8ae4e9ffdabff6ce0b156b1ac
kieferrm <kieferrm@gmail.com>
2016-09-14 11:35:37 -0700

    stop all containers

--- commands/stop-container.ts
+++ commands/stop-container.ts
@@ -4,22 +4,36 @@ import * as Docker from 'dockerode';
 
 
 interface Item extends vscode.QuickPickItem {
-    id: string
+    ids: string[]
 }
 
 function createItem(container: Docker.ContainerDesc) : Item {
     return <Item> {
         label: container.Image,
         description: container.Status,
-        id: container.Id
+        ids: [ container.Id ]
     };
 }
 
 function computeItems(containers: Docker.ContainerDesc[]) : vscode.QuickPickItem[] {
+
+    let allIds: string[] = [];
+
     let items : vscode.QuickPickItem[] = [];
     for (let i = 0; i < containers.length; i++) {
-        items.push(createItem(containers[i]));
+        let item = createItem(containers[i]);
+        allIds.push(item.ids[0]);
+        items.push(item);
     }
+
+    if (allIds.length > 0) {
+        items.unshift(<Item> {
+            label: 'All Containers',
+            description: 'Stops all running containers',
+            ids: allIds
+        });
+    }
+
     return items;
 }
 
@@ -31,11 +45,13 @@ export function stopContainer() {
             let items: vscode.QuickPickItem[] = computeItems(containers);
             vscode.window.showQuickPick(items, { placeHolder: 'Choose Container' }).then(function (selectedItem: Item) {
                 if (selectedItem) {
-                    let container = docker.getContainer(selectedItem.id);
-                    container.stop(function (err, data) {
-                        console.log("Stopped - error: " + err);
-                        console.log("Stopped - data: " + data);
-                    });
+                    for (let i = 0; i < selectedItem.ids.length; i++) {
+                        let container = docker.getContainer(selectedItem.ids[i]);
+                        container.stop(function (err, data) {
+                            console.log("Stopped - error: " + err);
+                            console.log("Stopped - data: " + data);
+                        });
+                    }
                 }
             });
         }
