61f73c46da36f53a6f2a98958faba3bd21c0884b
chrisdias <cdias@microsoft.com>
2016-10-12 12:05:36 -0700

    add support to remove all images

--- commands/remove-image.ts
+++ commands/remove-image.ts
@@ -1,15 +1,17 @@
-import {docker} from './utils/docker-endpoint';
-import {ImageItem, quickPickImage} from './utils/quick-pick-image';
+import { docker } from './utils/docker-endpoint';
+import { ImageItem, quickPickImage } from './utils/quick-pick-image';
 
 
 export function removeImage() {
     quickPickImage().then(function (selectedItem: ImageItem) {
         if (selectedItem) {
-            let image = docker.getImage(selectedItem.id);
-            image.remove({ force: true }, function (err, data) {
-                // console.log("Removed - error: " + err);
-                // console.log("Removed - data: " + data);
-            });
+            for (let i = 0; i < selectedItem.ids.length; i++) {
+                let image = docker.getImage(selectedItem.ids[i]);
+                image.remove({ force: true }, function (err, data) {
+                   // console.log("Removed - error: " + err);
+                   // console.log("Removed - data: " + data);
+                });
+            }
         }
     });
 }
--- commands/utils/quick-pick-image.ts
+++ commands/utils/quick-pick-image.ts
@@ -2,25 +2,37 @@ import * as Docker from 'dockerode';
 import {docker} from './docker-endpoint';
 import vscode = require('vscode');
 
-
-
 export interface ImageItem extends vscode.QuickPickItem {
-    id: string,
+    ids: string[],
 }
 
 function createItem(image: Docker.ImageDesc) : ImageItem {
     return <ImageItem> {
         label: image.RepoTags[0] || '<none>',
         description: null,
-        id: image.Id
+        ids: [image.Id]
     };
 }
 
-function computeItems(images: Docker.ImageDesc[]) : ImageItem[] {
+function computeItems(images: Docker.ImageDesc[], includeAll?: boolean) : ImageItem[] {
+    
+    let allIds: string[] = [];
+    
     let items : ImageItem[] = [];
     for (let i = 0; i < images.length; i++) {
-        items.push(createItem(images[i]));
+        let item = createItem(images[i]);
+        allIds.push(item.ids[0]);
+        items.push(item);
     }
+
+    if (includeAll && allIds.length > 0) {
+        items.unshift(<ImageItem> {
+            label: 'All Images',
+            description: 'Removes all images',
+            ids: allIds
+        });
+    }
+
     return items;
 }
 
@@ -30,7 +42,7 @@ export function quickPickImage() : Thenable<ImageItem> {
             vscode.window.showInformationMessage('There are no docker images yet. Try Build first.');
             return Promise.resolve(null);
         } else {
-            let items: ImageItem[] = computeItems(images);
+            let items: ImageItem[] = computeItems(images, true);
             return vscode.window.showQuickPick(items, { placeHolder: 'Choose image' });
         }
     });
