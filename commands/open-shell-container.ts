b5611ff41dab1424c2f5bdf8e83f58963cbfa2cb
Jonathan Carter <joncart@outlook.com>
2017-01-30 12:07:26 -0800

    Add Windows support for shell attach

--- commands/open-shell-container.ts
+++ commands/open-shell-container.ts
@@ -1,12 +1,20 @@
-import vscode = require('vscode');
+import * as vscode from 'vscode';
 import {ContainerItem, quickPickContainer} from './utils/quick-pick-container';
+import {DockerEngineType, docker} from './utils/docker-endpoint';
+
+const engineTypeShellCommands = {
+    [DockerEngineType.Linux]: "/bin/sh",
+    [DockerEngineType.Windows]: "cmd"
+}
 
 export function openShellContainer() {
-    quickPickContainer().then(function (selectedItem: ContainerItem) {
+    quickPickContainer().then((selectedItem: ContainerItem) => {
         if (selectedItem) {
-            let terminal = vscode.window.createTerminal(`sh ${selectedItem.label}`);
-            terminal.sendText(`docker exec -it ${selectedItem.ids[0]} /bin/sh`);
-            terminal.show();
+            docker.getEngineType().then((engineType: DockerEngineType) => {
+                const terminal = vscode.window.createTerminal(`Shell: ${selectedItem.label}`);
+                terminal.sendText(`docker exec -it ${selectedItem.ids[0]} ${engineTypeShellCommands[engineType]}`);
+                terminal.show();
+            });
         }
     });
 }
