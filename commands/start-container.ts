6dae8f620c198e1cc00ecbd905e530701d3663f4
Chris Dias <chris@diasfam.com>
2017-02-14 06:08:29 -0800

    make it work on windows

--- commands/start-container.ts
+++ commands/start-container.ts
@@ -25,35 +25,38 @@ export function startContainerInteractive() {
 }
 
 export function startAzureCLI() {
-    // block of we are on windows running windows containers... 
-    if (process.platform === 'win32') {
-        docker.getEngineType().then((engineType: DockerEngineType) => {
-            if (engineType === DockerEngineType.Windows) {
-                vscode.window.showErrorMessage<vscode.MessageItem>('Currently, you can only run the Azure CLI when running Linux based containers.',
-                    {
-                        title: 'More Information',
-                    },
-                    {
-                        title: 'Close',
-                        isCloseAffordance: true
-                    }
-                ).then((selected) => {
-                    if (!selected || selected.isCloseAffordance) {
-                        return;
-                    }
-                    return cp.exec('start https://docs.docker.com/docker-for-windows/#/switch-between-windows-and-linux-containers');
-                })
-            };
-        });
-    } else {
-        let option: string = process.platform === 'linux' ? '--net=host' : '';
-        // volume map .azure folder so don't have to log in every time
-        let vol: string = '-v ' + os.homedir() + '/.azure:~/.azure';
-        let cmd: string = `docker run ${option} ${vol} -it --rm azuresdk/azure-cli-python:latest`;
 
-        let terminal: vscode.Terminal = vscode.window.createTerminal('Azure CLI');
-        terminal.sendText(cmd);
-        terminal.show();
-        terminal.sendText(`az login`);
-    }
+    // block of we are running windows containers... 
+    docker.getEngineType().then((engineType: DockerEngineType) => {
+
+        if (engineType === DockerEngineType.Windows) {
+            vscode.window.showErrorMessage<vscode.MessageItem>('Currently, you can only run the Azure CLI when running Linux based containers.',
+                {
+                    title: 'More Information',
+                },
+                {
+                    title: 'Close',
+                    isCloseAffordance: true
+                }
+            ).then((selected) => {
+                if (!selected || selected.isCloseAffordance) {
+                    return;
+                }
+                return cp.exec('start https://docs.docker.com/docker-for-windows/#/switch-between-windows-and-linux-containers');
+            });
+        } else {
+            let option: string = process.platform === 'linux' ? '--net=host' : '';
+            
+            // volume map .azure folder so don't have to log in every time
+            let homeDir: string = process.platform === 'win32' ? os.homedir().replace(/\\/g, '/') : os.homedir();
+            let vol: string = '-v ' + homeDir + '/.azure:/root/.azure';
+            let cmd: string = `docker run ${option} ${vol} -it --rm azuresdk/azure-cli-python:latest`;
+
+            //docker run  -v c:/Users/cdias/.azure:/root/.azure -it --rm azuresdk/azure-cli-python:latest
+            let terminal: vscode.Terminal = vscode.window.createTerminal('Azure CLI');
+            terminal.sendText(cmd);
+            terminal.show();
+
+        }
+    });
 }
