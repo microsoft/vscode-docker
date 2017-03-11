73da8a5bf0739096f98094f3a5986aa37af4010e
Jonathan Carter <joncart@outlook.com>
2017-03-11 14:29:39 -0800

    Reorder imports

--- commands/docker-compose.ts
+++ commands/docker-compose.ts
@@ -1,5 +1,5 @@
-import * as vscode from 'vscode';
 import * as path from 'path';
+import * as vscode from 'vscode';
 import { COMPOSE_FILE_GLOB_PATTERN } from '../dockerExtension';
 
 function hasWorkspaceFolder() : boolean {
