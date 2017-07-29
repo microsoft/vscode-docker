3a33301d6bd6b638960392d67cd8c16bdbbb5b00
Jonathan Carter <joncart@outlook.com>
2017-07-29 15:18:36 -0700

    Add image inspect command

---
+++ documentContentProviders/dockerInspect.ts
@@ -0,0 +1,22 @@
+import { TextDocumentContentProvider, Uri, window, workspace } from "vscode";
+import { docker } from "../commands/utils/docker-endpoint";
+
+export const IMAGE_DOMAIN = "image";
+export const SCHEME = "docker-inspect";
+export const URI_EXTENSION = ".json";
+
+export default class DockerInspectDocumentContentProvider implements TextDocumentContentProvider {
+    static async openImageInspectDocument(imageName: string): Promise<void> {
+        const uri = Uri.parse(`${SCHEME}://${IMAGE_DOMAIN}/${imageName}${URI_EXTENSION}`);
+        window.showTextDocument(await workspace.openTextDocument(uri));
+    }
+
+    provideTextDocumentContent({ path }: Uri): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const imageName = path.substring(1).replace(URI_EXTENSION, "");
+            docker.getImage(imageName).inspect((error: Error, imageMetadata: any) => {
+                resolve(JSON.stringify(imageMetadata, null,  "    "));
+            });
+        });
+    }
+}
