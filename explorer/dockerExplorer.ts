bc9d816b6164575ffb91102ebd2dbf6d71cf5e2e
chrisdias <cdias@microsoft.com>
2017-08-02 15:04:54 -0700

    stash

--- explorer/dockerExplorer.ts
+++ explorer/dockerExplorer.ts
@@ -30,8 +30,8 @@ export class DockerExplorerProvider implements vscode.TreeDataProvider<DockerNod
         const nodes: DockerNode[] = [];
         if (!element) {
             nodes.push(new DockerNode("Images", vscode.TreeItemCollapsibleState.Collapsed, null, null));
-            nodes.push(new DockerNode("Running Containers", vscode.TreeItemCollapsibleState.Collapsed, null, null));
-            nodes.push(new DockerNode("Stopped Containers", vscode.TreeItemCollapsibleState.Collapsed, null, null));
+            nodes.push(new DockerNode("Containers", vscode.TreeItemCollapsibleState.Collapsed, null, null));
+            nodes.push(new DockerNode("Registries", vscode.TreeItemCollapsibleState.Collapsed, null, null));
         } else {
 
             if (element.label === 'Images') {
@@ -51,41 +51,38 @@ export class DockerExplorerProvider implements vscode.TreeDataProvider<DockerNod
                 }
             }
 
-            if (element.label === 'Running Containers') {
+            if (element.label === 'Containers') {
 
                 opts = {
                     "filters": {
-                        "status": ["created", "restarting", "running", "paused"]
+                        "status": ["created", "restarting", "running", "paused", "exited", "dead"]
                     }
                 };
+
+                var iconPath: any = {};
 
                 const containers: Docker.ContainerDesc[] = await docker.getContainerDescriptors(opts);
                 if (!containers || containers.length == 0) {
                     return [];
                 } else {
                     for (let i = 0; i < containers.length; i++) {
-                        nodes.push(new DockerNode(containers[i].Image + ' (' + containers[i].Status + ')', vscode.TreeItemCollapsibleState.None));
+                        if (['exited', 'dead'].includes(containers[i].State)) {
+                            // show stopped icon
+                            iconPath = {
+                                light: path.join(__filename, '..', '..', '..', 'images', 'light', 'mono_moby_small.png'),
+                                dark: path.join(__filename, '..', '..', '..', 'images', 'dark', 'mono_moby_small.png')
+                            }
+                        } else {
+                            iconPath = {
+                                light: path.join(__filename, '..', '..', '..', 'images', 'light', 'moby_small.png'),
+                                dark: path.join(__filename, '..', '..', '..', 'images', 'dark', 'moby_small.png')
+                            }
+                        }
+                        nodes.push(new DockerNode(containers[i].Image + ' [' + containers[i].Status + ']', vscode.TreeItemCollapsibleState.None, null, iconPath));
                     }
                 }
             }
 
-            if (element.label === 'Stopped Containers') {
-
-                opts = {
-                    "filters": {
-                        "status": ["exited", "dead"]
-                    }
-                };
-
-                const containers: Docker.ContainerDesc[] = await docker.getContainerDescriptors(opts);
-                if (!containers || containers.length == 0) {
-                    return [];
-                } else {
-                    for (let i = 0; i < containers.length; i++) {
-                        nodes.push(new DockerNode(containers[i].Image + ' (' + containers[i].Status + ')', vscode.TreeItemCollapsibleState.None));
-                    }
-                }
-            }
 
         }
 
@@ -129,21 +126,13 @@ class DockerNode extends vscode.TreeItem {
         public readonly command?: vscode.Command,
         public iconPath: any = {
             light: path.join(__filename, '..', '..', '..', 'images', 'light', 'mono_moby_small.png'),
-            dark: path.join(__filename, '..', '..', '..', 'images', 'dark', 'mono_moby_small.png')}
+            dark: path.join(__filename, '..', '..', '..', 'images', 'dark', 'mono_moby_small.png')
+        }
     ) {
         super(label, collapsibleState);
         this.iconPath = iconPath;
     }
 
-
-    // iconPath = {
-    //     light: path.join(__filename, '..', '..', '..', 'images', 'light', 'mono_moby_small.png'),
-    //     dark: path.join(__filename, '..', '..', '..', 'images', 'dark', 'mono_moby_small.png')
-    // };
-
-    // if(icon) {
-    //     iconPath = icon
-    // }
     contextValue = 'dockerImage';
 
 }
