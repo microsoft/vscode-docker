1f0c4ecc073957c79f4d35f94fdf32e78984966d
chrisdias <cdias@microsoft.com>
2017-09-18 17:14:53 -0700

    fixes #125

--- commands/system-prune.ts
+++ commands/system-prune.ts
@@ -1,19 +1,33 @@
 import vscode = require('vscode');
 import { reporter } from '../telemetry/telemetry';
 import { dockerExplorerProvider } from '../dockerExtension';
+import { docker } from './utils/docker-endpoint';
 
 const teleCmdId: string = 'vscode-docker.system.prune';
 
 export async function systemPrune() {
+    
+    const msg = 'Docker System Prune: Remove all unused containers, volumes, networks and images (both dangling and unreferenced)?';
+    const res = await vscode.window.showWarningMessage(msg, 'Yes', 'No');
+    if (res === 'Yes') {
+        const info = await docker.getEngineInfo();
+        const terminal = vscode.window.createTerminal("docker system prune");
+        const semver = require(`${vscode.env.appRoot}/node_modules/semver`);
 
-    const terminal = vscode.window.createTerminal("docker system prune");
-    terminal.sendText(`docker system prune -f`);
-    terminal.show();
-    dockerExplorerProvider.refreshContainers(true);
-    dockerExplorerProvider.refreshImages(true);
-    if (reporter) {
-        reporter.sendTelemetryEvent('command', {
-            command: teleCmdId
-        });
+        // in docker 17.06.1 and higher you must specify the --volumes flag
+        if (semver.gte(info.ServerVersion, '17.6.1', true)) {
+            terminal.sendText(`docker system prune --volumes -f`);
+        } else {
+            terminal.sendText(`docker system prune -f`);
+        }
+        
+        terminal.show();
+        dockerExplorerProvider.refreshContainers(true);
+        dockerExplorerProvider.refreshImages(true);
+        if (reporter) {
+            reporter.sendTelemetryEvent('command', {
+                command: teleCmdId
+            });
+        }
     }
 }
--- commands/utils/docker-endpoint.ts
+++ commands/utils/docker-endpoint.ts
@@ -6,36 +6,36 @@ export enum DockerEngineType {
 }
 
 class DockerClient {
-    private endPoint:Docker;
+    private endPoint: Docker;
 
     constructor() {
         // Pass no options so that the defaultOpts of docker-modem will be used
         this.endPoint = new Docker();
     }
 
-    public getContainerDescriptors(opts?: {}): Thenable<Docker.ContainerDesc[]>{
+    public getContainerDescriptors(opts?: {}): Thenable<Docker.ContainerDesc[]> {
         return new Promise((resolve, reject) => {
             if (!opts) {
                 let opts = {}
             }
-            
+
             this.endPoint.listContainers(opts, (err, containers) => {
                 if (err) {
-                    return reject(err); 
+                    return reject(err);
                 }
                 return resolve(containers);
             });
         });
     };
 
-    public getImageDescriptors(opts?: {}): Thenable<Docker.ImageDesc[]>{
+    public getImageDescriptors(opts?: {}): Thenable<Docker.ImageDesc[]> {
         return new Promise((resolve, reject) => {
             if (!opts) {
                 let opts = {}
             }
             this.endPoint.listImages(opts, (err, images) => {
                 if (err) {
-                    return reject(err); 
+                    return reject(err);
                 }
                 return resolve(images);
             });
@@ -46,7 +46,7 @@ class DockerClient {
         return this.endPoint.getContainer(id);
     }
 
-    public getEngineType() : Thenable<DockerEngineType> {
+    public getEngineType(): Thenable<DockerEngineType> {
         if (process.platform === 'win32') {
             return new Promise((resolve, reject) => {
                 this.endPoint.info((error, info) => {
@@ -64,16 +64,27 @@ class DockerClient {
         return Promise.resolve(DockerEngineType.Linux);
     }
 
-    public getExposedPorts(imageId: string) : Thenable<string[]> {
+    public getEngineInfo(): Thenable<any> {
         return new Promise((resolve, reject) => {
-            this.getImage(imageId).inspect((error, { Config: { ExposedPorts = {} }}) => {
+            this.endPoint.info((error, info) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(info);
+            });
+        });
+    }
+
+    public getExposedPorts(imageId: string): Thenable<string[]> {
+        return new Promise((resolve, reject) => {
+            this.getImage(imageId).inspect((error, { Config: { ExposedPorts = {} } }) => {
                 const ports = Object.keys(ExposedPorts).map((port) => port.split("/")[0]);
                 resolve(ports);
             });
         });
     }
 
-    public getImage(id:string): Docker.Image {
+    public getImage(id: string): Docker.Image {
         return this.endPoint.getImage(id);
     }
 }
