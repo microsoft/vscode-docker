fa06087a7272c0dc4a7bd125f030481313e5e9ee
chrisdias <cdias@microsoft.com>
2017-09-18 17:30:12 -0700

    handle prune command when docker not running

--- commands/system-prune.ts
+++ commands/system-prune.ts
@@ -10,20 +10,28 @@ export async function systemPrune() {
     const msg = 'Docker System Prune: Remove all unused containers, volumes, networks and images (both dangling and unreferenced)?';
     const res = await vscode.window.showWarningMessage(msg, 'Yes', 'No');
     if (res === 'Yes') {
-        const info = await docker.getEngineInfo();
         const terminal = vscode.window.createTerminal("docker system prune");
         const semver = require(`${vscode.env.appRoot}/node_modules/semver`);
 
-        // in docker 17.06.1 and higher you must specify the --volumes flag
-        if (semver.gte(info.ServerVersion, '17.6.1', true)) {
-            terminal.sendText(`docker system prune --volumes -f`);
-        } else {
-            terminal.sendText(`docker system prune -f`);
+        try {
+            const info = await docker.getEngineInfo();
+            
+            // in docker 17.06.1 and higher you must specify the --volumes flag
+            if (semver.gte(info.ServerVersion, '17.6.1', true)) {
+                terminal.sendText(`docker system prune --volumes -f`);
+            } else {
+                terminal.sendText(`docker system prune -f`);
+            }
+            
+            terminal.show();
+            dockerExplorerProvider.refreshContainers(true);
+            dockerExplorerProvider.refreshImages(true);
+
+        } catch (error) {
+            vscode.window.showErrorMessage('Unable to connect to Docker, is the Docker daemon running?');
+            console.log(error);            
         }
-        
-        terminal.show();
-        dockerExplorerProvider.refreshContainers(true);
-        dockerExplorerProvider.refreshImages(true);
+
         if (reporter) {
             reporter.sendTelemetryEvent('command', {
                 command: teleCmdId
