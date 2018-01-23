157199406137b10bb6b9daf9015bc5ba3ff04daa
Alessandro Segala <EgoAleSum@users.noreply.github.com>
2018-01-22 20:15:27 -0800

    Add package-lock.json to Node.js Docker template
    This will ensure that, when present, package-lock.json will be copied in the container and will be used for `npm install`.

--- configureWorkspace/configure.ts
+++ configureWorkspace/configure.ts
@@ -11,7 +11,7 @@ function genDockerFile(serviceName: string, platform: string, port: string, { cm
             return `FROM node:8.9-alpine
 ENV NODE_ENV production
 WORKDIR /usr/src/app
-COPY ["package.json", "npm-shrinkwrap.json*", "./"]
+COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 RUN npm install --production --silent && mv node_modules ../
 COPY . .
 EXPOSE ${port}
