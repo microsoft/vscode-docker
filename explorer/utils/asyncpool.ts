5d798028fa0b238636cd2e8720ccf136169c78ae
Esteban Rey <erey@stanford.edu>
2018-07-02 14:13:50 -0700

    Fixed networking connection limit bug
    While this is better, this is not quite the desired fix, desired fix is incoming.

---
+++ explorer/utils/asyncpool.ts
@@ -0,0 +1,31 @@
+/*Custom asyncpool
+ * To limit the number of asynchonous calls being done, this is helpful to limit 
+ * Connection requests and avoid throttling.
+ */
+export class asyncPool {
+    private runnableQueue: Function[];
+    private workers: Promise<void>[];
+    private asyncLim : number;
+
+    constructor(asyncLim: number) {
+        this.asyncLim = asyncLim;
+    }
+
+    public async scheduleBegin(asyncLim: number) {
+        for (let i = 0; i < this.asyncLim; i++) {
+            this.workers.push(this.worker());
+        }
+        await Promise.all(this.workers);
+    }
+
+    private async worker() {
+        while (this.runnableQueue.length > 0) {
+            let func = this.runnableQueue.pop();
+            await func();
+        }
+    }
+
+    public addTask(func: Function) {
+        this.runnableQueue.push(func);
+    }
+}
