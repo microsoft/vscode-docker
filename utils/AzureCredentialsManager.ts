711c584f1bc289507cef36afd115a062ae446e02
Esteban Rey <erey@stanford.edu>
2018-07-12 11:02:38 -0700

    Registry and account as well as client acquisition added

--- utils/AzureCredentialsManager.ts
+++ utils/AzureCredentialsManager.ts
@@ -6,6 +6,7 @@ import { ContainerRegistryManagementClient } from 'azure-arm-containerregistry';
 import { AzureAccountWrapper } from '.././explorer/deploy/azureAccountWrapper';
 import { RegistryRootNode } from "../explorer/models/registryRootNode";
 import { RegistryNameStatus } from "azure-arm-containerregistry/lib/models";
+import * as ContainerModels from '../node_modules/azure-arm-containerregistry/lib/models';
 import { ResourceGroup, ResourceGroupListResult } from "azure-arm-resource/lib/resource/models";
 
 const MAX_CONCURRENT_REQUESTS = 8;
@@ -30,6 +31,10 @@ export class AzureCredentialsManager {
         this.azureAccount = azureAccount;
     }
 
+    public getAccount() {
+        return this.azureAccount;
+    }
+
     public getFilteredSubscriptionList(): SubscriptionModels.Subscription[] {
         return this.azureAccount.filters.map<SubscriptionModels.Subscription>(filter => {
             return {
@@ -45,13 +50,48 @@ export class AzureCredentialsManager {
         });
     }
 
-    public getRegistries() {
+    public getContainerRegistryManagementClient(subscription: SubscriptionModels.Subscription): ContainerRegistryManagementClient {
+        return new ContainerRegistryManagementClient(this.getCredentialByTenantId(subscription.tenantId), subscription.subscriptionId);
+    }
 
+    public async getRegistries(subscription?: SubscriptionModels.Subscription, resourceGroup?: string, sortFunction?): Promise<ContainerModels.Registry[]> {
+        let registries: ContainerModels.Registry[] = [];
+
+        if (subscription && resourceGroup) {
+            //Get all registries under one resourcegroup
+            const client = this.getContainerRegistryManagementClient(subscription);
+            registries = await client.registries.listByResourceGroup(resourceGroup);
+
+        } else if (subscription) {
+            //Get all registries under one subscription
+            const client = this.getContainerRegistryManagementClient(subscription);
+            registries = await client.registries.list();
+
+        } else {
+            //Get all registries for all subscriptions
+            const subs: SubscriptionModels.Subscription[] = this.getFilteredSubscriptionList();
+            const subPool = new AsyncPool(MAX_CONCURRENT_SUBSCRIPTON_REQUESTS);
+
+            for (let i = 0; i < subs.length; i++) {
+                subPool.addTask(async () => {
+                    const client = new ContainerRegistryManagementClient(this.getCredentialByTenantId(subs[i].tenantId), subs[i].subscriptionId);
+                    let registry: ContainerModels.Registry[] = await client.registries.list();
+                    registries.concat(registry);
+                });
+            }
+            await subPool.scheduleRun();
+        }
+
+        if (sortFunction) {
+            registries.sort(sortFunction);
+        }
+
+        return registries;
     }
 
     public async getResourceGroups(subscription?: SubscriptionModels.Subscription): Promise<ResourceGroup[]> {
         if (subscription) {
-            const resourceClient = new ResourceManagementClient(this.getCredentialByTenantId(subscription.tenantId, this.azureAccount), subscription.subscriptionId);
+            const resourceClient = new ResourceManagementClient(this.getCredentialByTenantId(subscription.tenantId), subscription.subscriptionId);
             return await resourceClient.resourceGroups.list();
         }
         const subs = this.getFilteredSubscriptionList();
@@ -60,7 +100,7 @@ export class AzureCredentialsManager {
         //Acquire each subscription's data simultaneously
         for (let i = 0; i < subs.length; i++) {
             subPool.addTask(async () => {
-                const resourceClient = new ResourceManagementClient(this.getCredentialByTenantId(subscription.tenantId, this.azureAccount), subscription.subscriptionId);
+                const resourceClient = new ResourceManagementClient(this.getCredentialByTenantId(subscription.tenantId), subscription.subscriptionId);
                 const internalGroups = await resourceClient.resourceGroups.list();
                 resourceGroups.concat(resourceGroups);
             });
@@ -69,9 +109,9 @@ export class AzureCredentialsManager {
         return resourceGroups;
     }
 
-    private getCredentialByTenantId(tenantId: string, azureAccount: AzureAccount): ServiceClientCredentials {
+    public getCredentialByTenantId(tenantId: string): ServiceClientCredentials {
 
-        const session = azureAccount.sessions.find((s, i, array) => s.tenantId.toLowerCase() === tenantId.toLowerCase());
+        const session = this.azureAccount.sessions.find((s, i, array) => s.tenantId.toLowerCase() === tenantId.toLowerCase());
 
         if (session) {
             return session.credentials;
@@ -79,4 +119,6 @@ export class AzureCredentialsManager {
 
         throw new Error(`Failed to get credentials, tenant ${tenantId} not found.`);
     }
+
+
 }
