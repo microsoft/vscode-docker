5d4491cb7c54b24e65181ce2af1a599139163bcd
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-07-17 14:36:28 -0700

    fix new code

--- utils/asyncpool.ts
+++ utils/asyncpool.ts
@@ -17,7 +17,7 @@ export class AsyncPool {
     /*Runs all functions in runnableQueue by launching asyncLimit worker instances
       each of which calls an async task extracted from runnableQueue. This will
       wait for all scheduled tasks to be completed.*/
-    public async runAll() {
+    public async runAll(): Promise<void> {
         for (let i = 0; i < this.asyncLimit; i++) {
             this.workers.push(this.worker());
         }
@@ -29,13 +29,13 @@ export class AsyncPool {
     }
 
     /*Takes in an async Thunk to be executed by the asyncpool*/
-    public addTask(func: Function) {
+    public addTask(func: Function): void {
         this.runnableQueue.push(func);
     }
 
     /*Executes each passed in async function blocking while each function is run.
       Moves on to the next available thunk on completion of the previous thunk.*/
-    private async worker() {
+    private async worker(): Promise<void> {
         while (this.runnableQueue.length > 0) {
             let func = this.runnableQueue.pop();
             //Avoids possible race condition
