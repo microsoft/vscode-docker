ea78dc7567dfcf065ef2c39b485f2234a069fd0b
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2018-07-17 17:34:45 -0700

    Add tests for 'configure' (Add docker files to workspace) (#303)
    * Add tests for 'configure'
    
    * Python
    
    * Linux
    
    * work
    
    * work
    
    * Fix casing
    
    * clean-up
    
    * Remove randomness.ts, fix assert message
    
    * Update to 0.1.0-alpha
    
    * Revert "Update to 0.1.0-alpha"
    
    This reverts commit f17534028e7437842b33ba91fc304e4bb1ecfd22.
    
    * PR comments
    
    * fix lint
    
    * fix lint
    
    * fix build
    
    * increase timeout

--- test/index.ts
+++ test/index.ts
@@ -19,9 +19,14 @@ var testRunner = require('vscode/lib/testrunner');
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
+let options = {
     ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
     useColors: true // colored output from test results
-});
+};
+let noTimeouts = process.env.NO_TIMEOUTS;
+if (!!noTimeouts && noTimeouts !== "0") {
+    options["timeout"] = Number.MAX_SAFE_INTEGER;
+}
+testRunner.configure(options);
 
 module.exports = testRunner;
