b71360045ec5530171f34204383242852caf996c
Prashanth <prsri@microsoft.com>
2018-07-19 15:57:40 -0700

    Remove UserCancelled error -vscode quickpick to azuretools ui

--- commands/docker-compose.ts
+++ commands/docker-compose.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
 import { COMPOSE_FILE_GLOB_PATTERN } from '../dockerExtension';
+import { ext } from '../extensionVariables';
 import { reporter } from '../telemetry/telemetry';
 import { createTerminal } from './utils/create-terminal';
 const teleCmdId: string = 'vscode-docker.compose.'; // we append up or down when reporting telemetry
@@ -67,31 +68,29 @@ async function compose(commands: string[], message: string, dockerComposeFileUri
         }
 
         const items: vscode.QuickPickItem[] = computeItems(folder, uris);
-        selectedItems.push(<Item>await vscode.window.showQuickPick(items, { placeHolder: `Choose Docker Compose file ${message}` }));
+        selectedItems.push(<Item>await ext.ui.showQuickPick(items, { placeHolder: `Choose Docker Compose file ${message}` }));
     }
 
-    if (selectedItems.length > 0) {
-        const terminal: vscode.Terminal = createTerminal('Docker Compose');
-        const configOptions: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('docker');
-        const build: string = configOptions.get('dockerComposeBuild', true) ? '--build' : '';
-        const detached: string = configOptions.get('dockerComposeDetached', true) ? '-d' : '';
+    const terminal: vscode.Terminal = createTerminal('Docker Compose');
+    const configOptions: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('docker');
+    const build: string = configOptions.get('dockerComposeBuild', true) ? '--build' : '';
+    const detached: string = configOptions.get('dockerComposeDetached', true) ? '-d' : '';
 
-        terminal.sendText(`cd "${folder.uri.fsPath}"`);
-        for (let command of commands) {
-            selectedItems.forEach((item: Item) => {
-                terminal.sendText(command.toLowerCase() === 'up' ? `docker-compose -f ${item.file} ${command} ${detached} ${build}` : `docker-compose -f ${item.file} ${command}`);
+    terminal.sendText(`cd "${folder.uri.fsPath}"`);
+    for (let command of commands) {
+        selectedItems.forEach((item: Item) => {
+            terminal.sendText(command.toLowerCase() === 'up' ? `docker-compose -f ${item.file} ${command} ${detached} ${build}` : `docker-compose -f ${item.file} ${command}`);
+        });
+        terminal.show();
+        if (reporter) {
+            /* __GDPR__
+               "command" : {
+                  "command" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" }
+               }
+             */
+            reporter.sendTelemetryEvent('command', {
+                command: teleCmdId + command
             });
-            terminal.show();
-            if (reporter) {
-                /* __GDPR__
-                   "command" : {
-                      "command" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" }
-                   }
-                 */
-                reporter.sendTelemetryEvent('command', {
-                    command: teleCmdId + command
-                });
-            }
         }
     }
 
