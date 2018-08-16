bf48a2fe25c81d5a8200dcbc35e65ac48a23ab32
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-08-16 16:25:02 -0700

    fix lint

--- explorer/models/registryType.ts
+++ explorer/models/registryType.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-
 export enum RegistryType {
     DockerHub,
     Azure,
