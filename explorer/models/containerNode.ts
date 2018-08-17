9c7c7656fc43fba28777d82377b72c21f52d377a
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2018-08-16 17:17:28 -0700

    Copyright headers (#385)
    * Copyright headers
    
    * fix lint

--- explorer/models/containerNode.ts
+++ explorer/models/containerNode.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as vscode from 'vscode';
 import { trimWithElipsis } from '../utils/utils';
 import { NodeBase } from './nodeBase';
--- explorer/models/registryRootNode.ts
+++ explorer/models/registryRootNode.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as ContainerModels from 'azure-arm-containerregistry/lib/models';
 import { SubscriptionModels } from 'azure-arm-resource';
 import * as keytarType from 'keytar';
