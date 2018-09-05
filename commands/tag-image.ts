32904f43a90e0a096b413175f8439ffd3a138423
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-09-04 17:11:38 -0700

    whitespace

--- commands/tag-image.ts
+++ commands/tag-image.ts
@@ -105,7 +105,7 @@ export async function getOrAskForImageAndTag(actionContext: IActionContext, cont
 
 const KnownRegistries: { type: string, regex: RegExp }[] = [
     // Like username/path
-    { type: 'dockerhub-namespace', regex: /^[^.:]+\/[^.:] + \/$/ },
+    { type: 'dockerhub-namespace', regex: /^[^.:]+\/[^.:]+\/$/ },
 
     { type: 'dockerhub-dockerio', regex: /^docker.io.*\// },
     { type: 'gitlab', regex: /gitlab.*\// },
