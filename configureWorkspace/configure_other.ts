427df96ef0144ec6aded49b1b0d115f45aafca61
Stephen Weatherford <stephwe@microsoft.com>
2018-09-24 16:27:54 -0700

    Fix 'Other' platform generation (hotfix 0.3.1)

--- configureWorkspace/configure_other.ts
+++ configureWorkspace/configure_other.ts
@@ -5,41 +5,18 @@
 
 import { PackageInfo } from './configure';
 
-export let configurePython = {
+export let configureOther = {
   genDockerFile,
   genDockerCompose,
-  genDockerComposeDebug
+  genDockerComposeDebug,
+  defaultPort: '3000'
 };
 
 function genDockerFile(serviceNameAndRelativePath: string, platform: string, os: string | undefined, port: string, { cmd, author, version, artifactName }: Partial<PackageInfo>): string {
-  return `# Python support can be specified down to the minor or micro version
-# (e.g. 3.6 or 3.6.3).
-# OS Support also exists for jessie & stretch (slim and full).
-# See https://hub.docker.com/r/library/python/ for all supported Python
-# tags from Docker Hub.
-FROM python:alpine
-
-# If you prefer miniconda:
-#FROM continuumio/miniconda3
-
+  return `FROM docker/whalesay:latest
 LABEL Name=${serviceNameAndRelativePath} Version=${version}
-EXPOSE ${port}
-
-WORKDIR /app
-ADD . /app
-
-# Using pip:
-RUN python3 -m pip install -r requirements.txt
-CMD ["python3", "-m", "${serviceNameAndRelativePath}"]
-
-# Using pipenv:
-#RUN python3 -m pip install pipenv
-#RUN pipenv install --ignore-pipfile
-#CMD ["pipenv", "run", "python3", "-m", "${serviceNameAndRelativePath}"]
-
-# Using miniconda (make sure to replace 'myenv' w/ your environment name):
-#RUN conda env create -f environment.yml
-#CMD /bin/bash -c "source activate myenv && python3 -m ${serviceNameAndRelativePath}"
+RUN apt-get -y update && apt-get install -y fortunes
+CMD /usr/games/fortune -a | cowsay
 `;
 }
 
