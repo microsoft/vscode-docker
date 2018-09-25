aa62c93bd072c2c8cd4d779eca2582cf689f8bf5
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2018-09-25 11:42:14 -0700

    Fix 'Other' platform generation (hotfix 0.3.1) (#509) (#514)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.3.1 - 25 September 2018
+
+### Fixed
+
+* Error while generating Dockerfile for 'other' [#504](https://github.com/Microsoft/vscode-docker/issues/504)
+
 ## 0.3.0 - 21 September 2018
 
 ### Added
--- configureWorkspace/configure_other.ts
+++ configureWorkspace/configure_other.ts
@@ -3,9 +3,9 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { getExposeStatements, IPlatformGeneratorInfo, PackageInfo } from './configure';
+import { PackageInfo } from './configure';
 
-export let configurePython: IPlatformGeneratorInfo = {
+export let configureOther = {
   genDockerFile,
   genDockerCompose,
   genDockerComposeDebug,
@@ -13,36 +13,10 @@ export let configurePython: IPlatformGeneratorInfo = {
 };
 
 function genDockerFile(serviceNameAndRelativePath: string, platform: string, os: string | undefined, port: string, { cmd, author, version, artifactName }: Partial<PackageInfo>): string {
-  let exposeStatements = getExposeStatements(port);
-
-  return `# Python support can be specified down to the minor or micro version
-# (e.g. 3.6 or 3.6.3).
-# OS Support also exists for jessie & stretch (slim and full).
-# See https://hub.docker.com/r/library/python/ for all supported Python
-# tags from Docker Hub.
-FROM python:alpine
-
-# If you prefer miniconda:
-#FROM continuumio/miniconda3
-
+  return `FROM docker/whalesay:latest
 LABEL Name=${serviceNameAndRelativePath} Version=${version}
-${exposeStatements}
-
-WORKDIR /app
-ADD . /app
-
-# Using pip:
-RUN python3 -m pip install -r requirements.txt
-CMD ["python3", "-m", "${serviceNameAndRelativePath}"]
-
-# Using pipenv:
-#RUN python3 -m pip install pipenv
-#RUN pipenv install --ignore-pipfile
-#CMD ["pipenv", "run", "python3", "-m", "${serviceNameAndRelativePath}"]
-
-# Using miniconda (make sure to replace 'myenv' w/ your environment name):
-#RUN conda env create -f environment.yml
-#CMD /bin/bash -c "source activate myenv && python3 -m ${serviceNameAndRelativePath}"
+RUN apt-get -y update && apt-get install -y fortunes
+CMD /usr/games/fortune -a | cowsay
 `;
 }
 
