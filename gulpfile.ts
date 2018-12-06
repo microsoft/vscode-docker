a29573dcb36a738727419d11d1a8ea094794ac7e
Eric Jizba <erijiz@microsoft.com>
2018-12-06 13:42:49 -0800

    Clean up gulp/pipelines and re-enable mac tests (#685)
    Actual fixes:
    1. Always use Node 8. I think this is the key thing that fixed webpack on macOS and allowed me to pretty easily re-enable the tests (since Stephen already did the "DOCKER_UNAVAILABLE" stuff)
    1. Don't use `async function` because it meant test failures weren't being reported.
    
    Other stuff that was just nice to have:
    1. Consolidate into one `common\test.yml`
    1. Use typescript for gulpfile (enabled with `ts-node`)
    1. Use functions instead of `gulp.task` which is deprecated

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,36 +3,37 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const gulp = require('gulp');
-const decompress = require('gulp-decompress');
-const download = require('gulp-download');
-const path = require('path');
-const fse = require('fs-extra');
-const os = require('os');
-const cp = require('child_process');
-const glob = require('glob');
+// Grandfathered in
+// tslint:disable:typedef
+// tslint:disable:no-unsafe-any
+
+import * as cp from 'child_process';
+import * as fse from 'fs-extra';
+import * as glob from 'glob';
+import * as gulp from 'gulp';
+import * as decompress from 'gulp-decompress';
+import * as download from 'gulp-download';
+import * as os from 'os';
+import * as path from 'path';
 
 const env = process.env;
 
-gulp.task('webpack-dev', async () => {
-    preWebpack();
-    return spawn(path.join(__dirname, './node_modules/.bin/webpack'), ['--mode', 'development'], { stdio: 'inherit', env });
-});
-
-gulp.task('webpack-prod', async () => {
-    preWebpack();
-    return spawn(path.join(__dirname, './node_modules/.bin/webpack'), ['--mode', 'production'], { stdio: 'inherit', env });
-});
+function webpack(mode) {
+    // without this, webpack can run out of memory in some environments
+    env.NODE_OPTIONS = '--max-old-space-size=8192';
+    return spawn(path.join(__dirname, './node_modules/.bin/webpack'), ['--mode', mode], { stdio: 'inherit', env });
+}
 
 /**
  * Installs the azure account extension before running tests (otherwise our extension would fail to activate)
  * NOTE: The version isn't super important since we don't actually use the account extension in tests
  */
-gulp.task('install-azure-account', async () => {
+function installAzureAccount() {
     const version = '0.4.3';
     const extensionPath = path.join(os.homedir(), `.vscode/extensions/ms-vscode.azure-account-${version}`);
     const existingExtensions = glob.sync(extensionPath.replace(version, '*'));
     if (existingExtensions.length === 0) {
+        // tslint:disable-next-line:no-http-string
         return download(`http://ms-vscode.gallery.vsassets.io/_apis/public/gallery/publisher/ms-vscode/extension/azure-account/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`)
             .pipe(decompress({
                 filter: file => file.path.startsWith('extension/'),
@@ -44,15 +45,16 @@ gulp.task('install-azure-account', async () => {
             .pipe(gulp.dest(extensionPath));
     } else {
         console.log('Azure Account extension already installed.');
+        return Promise.resolve();
     }
-});
+}
 
-gulp.task('test', gulp.series('install-azure-account', async () => {
-    env.DEBUGTELEMETRY = 1;
+function test() {
+    env.DEBUGTELEMETRY = '1';
     env.CODE_TESTS_WORKSPACE = path.join(__dirname, 'test/test.code-workspace');
     env.CODE_TESTS_PATH = path.join(__dirname, 'dist/test');
     return spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
-}));
+}
 
 function spawn(command, args, options) {
     if (process.platform === 'win32') {
@@ -61,13 +63,11 @@ function spawn(command, args, options) {
         } else if (fse.pathExistsSync(command + '.cmd')) {
             command = command + '.cmd';
         }
-
     }
 
     return cp.spawn(command, args, options);
 }
 
-function preWebpack() {
-    // without this, webpack can run out of memory in some environments
-    env.NODE_OPTIONS = '--max-old-space-size=8192';
-}
+exports['webpack-dev'] = () => webpack('development');
+exports['webpack-prod'] = () => webpack('production');
+exports.test = gulp.series(installAzureAccount, test);
