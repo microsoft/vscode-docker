eea75edf188a72b6bc988e720acf5b7057141a47
Phillip Hoff <phillip@orst.edu>
2019-01-30 15:53:40 -0800

    Sketch alpine debugging support.

--- debugging/coreclr/debuggerClient.ts
+++ debugging/coreclr/debuggerClient.ts
@@ -4,22 +4,39 @@
 
 import { PlatformOS } from '../../utils/platform';
 import { VsDbgClient } from './vsdbgClient';
+import { DockerClient } from './dockerClient';
 
 export interface DebuggerClient {
-    getDebugger(os: PlatformOS): Promise<string>;
+    getDebugger(os: PlatformOS, containerId: string): Promise<string>;
+    getDebuggerFolder(): Promise<string>;
 }
 
 export class DefaultDebuggerClient {
     private static debuggerVersion: string = 'vs2017u5';
-    private static debuggerLinuxRuntime: string = 'debian.8-x64';
+    private static debuggerLinuxAlpineRuntime: string = 'linux-musl-x64';
+    private static debuggerLinuxDefaultRuntime: string = 'linux-x64';
     private static debuggerWindowsRuntime: string = 'win7-x64';
 
-    constructor(private readonly vsdbgClient: VsDbgClient) {
+    constructor(
+        private readonly dockerClient: DockerClient,
+        private readonly vsdbgClient: VsDbgClient) {
     }
 
-    public async getDebugger(os: PlatformOS): Promise<string> {
-        return await this.vsdbgClient.getVsDbgVersion(
-            DefaultDebuggerClient.debuggerVersion,
-            os === 'Windows' ? DefaultDebuggerClient.debuggerWindowsRuntime : DefaultDebuggerClient.debuggerLinuxRuntime);
+    public async getDebugger(os: PlatformOS, containerId: string): Promise<string> {
+        if (os === 'Windows') {
+            return await this.vsdbgClient.getVsDbgVersion(DefaultDebuggerClient.debuggerVersion, DefaultDebuggerClient.debuggerWindowsRuntime);
+        } else {
+            const result = await this.dockerClient.exec(containerId, '/bin/sh -c \'. /etc/os-release && echo $ID\'', { interactive: true });
+
+            return await this.vsdbgClient.getVsDbgVersion(
+                DefaultDebuggerClient.debuggerVersion,
+                result.trim() === 'alpine'
+                    ? DefaultDebuggerClient.debuggerLinuxAlpineRuntime
+                    : DefaultDebuggerClient.debuggerLinuxDefaultRuntime);
+        }
+    }
+
+    public getDebuggerFolder(): Promise<string> {
+        return this.vsdbgClient.getVsDbgFolder();
     }
 }
