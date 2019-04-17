2ee3be061da53663ee04e6626c406094d215c65b
Prashanth <prsri@microsoft.com>
2019-04-16 18:52:11 -0700

    vscode.env.openExternal -> vscode.open

--- explorer/utils/openExternal.ts
+++ explorer/utils/openExternal.ts
@@ -3,11 +3,12 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { env, Uri } from 'vscode';
+import { commands, Uri } from 'vscode';
+import { VersionedTextDocumentIdentifier } from 'vscode-languageclient';
 
 export async function openExternal(path: string, throwErrorOnFailure: boolean = false): Promise<void> {
     let uri = Uri.parse(path);
-    let successful: boolean = await env.openExternal(uri);
+    let successful: boolean = await commands.executeCommand('vscode.open', uri);
     if (!successful && throwErrorOnFailure) {
         throw new Error(`Opening ${path} was unsuccessful`);
     }
