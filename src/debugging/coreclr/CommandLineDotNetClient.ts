fe9bd037905c5615f54671467aa70d4d7d1239c3
Brandon Waterloo <36966225+bwateratmsft@users.noreply.github.com>
2019-07-19 11:13:44 -0400

    Spawn a trust prompt on Mac and Windows

--- src/debugging/coreclr/CommandLineDotNetClient.ts
+++ src/debugging/coreclr/CommandLineDotNetClient.ts
@@ -2,10 +2,13 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
+import * as cp from 'child_process';
 import * as semver from 'semver';
 import { v4 as uuidv4 } from 'uuid';
+import { window } from 'vscode';
 import { ProcessProvider } from "./ChildProcessProvider";
 import { FileSystemProvider } from "./fsProvider";
+import { OSProvider } from "./LocalOSProvider";
 
 export type MSBuildExecOptions = {
     target?: string;
@@ -23,7 +26,8 @@ export class CommandLineDotNetClient implements DotNetClient {
 
     constructor(
         private readonly processProvider: ProcessProvider,
-        private readonly fsProvider: FileSystemProvider) {
+        private readonly fsProvider: FileSystemProvider,
+        private readonly osProvider: OSProvider) {
     }
 
     public async execTarget(projectFile: string, options?: MSBuildExecOptions): Promise<void> {
@@ -64,20 +68,42 @@ export class CommandLineDotNetClient implements DotNetClient {
 
         await this.addUserSecretsIfNecessary(projectFile);
 
+        if (this.osProvider.os === 'Windows' || this.osProvider.isMac) {
+            try {
+                // Check if the certificate is already trusted
+                const checkCommand = `dotnet dev-certs https --check`;
+                await this.processProvider.exec(checkCommand, {});
+            } catch {
+                // It is not trusted--ask if they want to do so
+                window.showWarningMessage(
+                    "The ASP.NET Core HTTPS development certificate is not trusted. Would you like to trust the certificate? A prompt may be shown.",
+                    { modal: false },
+                    ...['Yes', 'No']).then(selection => {
+                        if (selection === 'Yes') {
+                            const trustCommand = `dotnet dev-certs https --trust`;
+                            cp.spawn(trustCommand, { shell: true, windowsHide: false, detached: true });
+                        }
+                    });
+            }
+        }
+
         const password = uuidv4();
 
+        // Export the certificate
         const exportCommand = `dotnet dev-certs https -ep "${hostExportPath}" -p "${password}"`;
         await this.processProvider.exec(exportCommand, {});
 
+        // Set the password to dotnet user-secrets
         const userSecretsPasswordCommand = `dotnet user-secrets --project "${projectFile}" set Kestrel:Certificates:Development:Password "${password}"`;
         await this.processProvider.exec(userSecretsPasswordCommand, {});
-
-        CommandLineDotNetClient.KnownConfiguredProjects.add(projectFile);
 
         // This is not honored due to https://github.com/aspnet/AspNetCore.Docs/issues/6199#issuecomment-418194220
         // Consequently, the certificate name must be equal to <binaryName>.pfx, i.e. MyWebApp.dll => MyWebApp.pfx
         //const userSecretsPathCommand = `dotnet user-secrets --project "${projectFile}" set Kestrel:Certificates:Development:Path "${containerExportPath}"`;
         //await this.processProvider.exec(userSecretsPathCommand, {});
+
+        // Cache the project so we don't do this all over again every F5
+        CommandLineDotNetClient.KnownConfiguredProjects.add(projectFile);
     }
 
     private async addUserSecretsIfNecessary(projectFile: string): Promise<void> {
