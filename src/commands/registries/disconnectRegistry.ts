77efd83d9b25c64c02f653546c127feacb369c4e
Eric Jizba <erijiz@microsoft.com>
2019-07-25 11:48:32 -0700

    Make disconnect registry more robust to errors

--- src/commands/registries/disconnectRegistry.ts
+++ src/commands/registries/disconnectRegistry.ts
@@ -6,12 +6,7 @@
 import { AzExtTreeItem, IActionContext } from "vscode-azureextensionui";
 import { ext } from "../../extensionVariables";
 import { IRegistryProviderTreeItem } from "../../tree/registries/IRegistryProviderTreeItem";
-import { registryExpectedContextValues } from "../../tree/registries/registryContextValues";
 
 export async function disconnectRegistry(context: IActionContext, node?: IRegistryProviderTreeItem & AzExtTreeItem): Promise<void> {
-    if (!node) {
-        node = await ext.registriesTree.showTreeItemPicker<IRegistryProviderTreeItem & AzExtTreeItem>(registryExpectedContextValues.all.registryProvider, context);
-    }
-
-    await ext.registriesRoot.disconnectRegistry(context, node);
+    await ext.registriesRoot.disconnectRegistry(context, node && node.cachedProvider);
 }
