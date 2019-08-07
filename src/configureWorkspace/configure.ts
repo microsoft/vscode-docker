01023e384e605bd939c3d524421d091760481e9c
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2019-08-07 13:44:05 -0400

    Update .dockerignore to match VS (#1187)
    * Update .dockerignore to match VS
    
    * Alphabetize the list
    
    * Add Java related dockerignore items

--- src/configureWorkspace/configure.ts
+++ src/configureWorkspace/configure.ts
@@ -113,19 +113,31 @@ function genDockerComposeDebug(serviceNameAndRelativePath: string, platform: Pla
 }
 
 function genDockerIgnoreFile(service: string, platformType: string, os: string, ports: number[]): string {
-    return `node_modules
-npm-debug.log
-Dockerfile*
-docker-compose*
-.dockerignore
-.git
-.gitignore
-.env
-*/bin
-*/obj
-README.md
+    return `**/.classpath
+**/.dockerignore
+**/.env
+**/.git
+**/.gitignore
+**/.project
+**/.settings
+**/.toolstarget
+**/.vs
+**/.vscode
+**/*.*proj.user
+**/*.dbmdl
+**/*.jfm
+**/azds.yaml
+**/bin
+**/charts
+**/docker-compose*
+**/Dockerfile*
+**/node_modules
+**/npm-debug.log
+**/obj
+**/secrets.dev.yaml
+**/values.dev.yaml
 LICENSE
-.vscode`;
+README.md`;
 }
 
 async function getPackageJson(folderPath: string): Promise<vscode.Uri[]> {
