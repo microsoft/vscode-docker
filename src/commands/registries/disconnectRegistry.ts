95bf77690b184510eafa3aa2afcef96cc52b104b
Eric Jizba <erijiz@microsoft.com>
2019-08-08 10:38:28 -0700

    Allow user to disconnect invalid registry from tree

--- src/commands/registries/disconnectRegistry.ts
+++ src/commands/registries/disconnectRegistry.ts
@@ -3,10 +3,17 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AzExtTreeItem, IActionContext } from "vscode-azureextensionui";
+import { IActionContext, InvalidTreeItem } from "vscode-azureextensionui";
 import { ext } from "../../extensionVariables";
+import { ICachedRegistryProvider } from "../../tree/registries/ICachedRegistryProvider";
 import { IRegistryProviderTreeItem } from "../../tree/registries/IRegistryProviderTreeItem";
 
-export async function disconnectRegistry(context: IActionContext, node?: IRegistryProviderTreeItem & AzExtTreeItem): Promise<void> {
-    await ext.registriesRoot.disconnectRegistry(context, node && node.cachedProvider);
+export async function disconnectRegistry(context: IActionContext, node?: InvalidTreeItem | IRegistryProviderTreeItem): Promise<void> {
+    let cachedProvider: ICachedRegistryProvider | undefined;
+    if (node instanceof InvalidTreeItem) {
+        cachedProvider = <ICachedRegistryProvider>node.data;
+    } else if (node) {
+        cachedProvider = node.cachedProvider;
+    }
+    await ext.registriesRoot.disconnectRegistry(context, cachedProvider);
 }
