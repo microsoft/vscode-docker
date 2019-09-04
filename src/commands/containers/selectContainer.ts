f063460deabea5b4621305dfdd2b4e0593180dac
Simon Le Bourdais-Cabana <simonlbc@gmail.com>
2019-09-04 18:28:14 -0400

    Fix PR comment - cleanup regex

--- src/commands/containers/selectContainer.ts
+++ src/commands/containers/selectContainer.ts
@@ -14,7 +14,7 @@ export async function selectContainer(context: IActionContext): Promise<string> 
     //Expecting running containers to change often as this is a debugging scenario.
     await ext.containersTree.refresh();
 
-    node = await ext.containersTree.showTreeItemPicker(/^(running)Container$/i, { ...context });
+    node = await ext.containersTree.showTreeItemPicker(/^runningContainer$/i, { ...context });
 
     return node.containerId;
 }
