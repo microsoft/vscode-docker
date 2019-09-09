5d680dac90410b6ba0a846c9ec81a8fbea43b725
Brandon Waterloo <36966225+bwateratmsft@users.noreply.github.com>
2019-09-09 13:42:55 -0400

    Explanatory comments

--- src/utils/resolveFilePath.ts
+++ src/utils/resolveFilePath.ts
@@ -7,6 +7,11 @@ import * as os from 'os';
 import * as path from 'path';
 import { WorkspaceFolder } from 'vscode';
 
+/**
+ * Given a file path, tries to determine an absolute path. If relative, path is resolved relative to workspace folder.
+ * @param filePath The file path to resolve
+ * @param folder The workspace folder
+ */
 export function resolveFilePath(filePath: string, folder: WorkspaceFolder): string {
     let replacedPath = filePath.replace(/\$\{workspaceFolder\}/gi, folder.uri.fsPath);
 
@@ -19,6 +24,11 @@ export function resolveFilePath(filePath: string, folder: WorkspaceFolder): stri
     return path.resolve(folder.uri.fsPath, replacedPath);
 }
 
+/**
+ * Given an absolute file path, tries to make it relative to the workspace folder.
+ * @param filePath The file path to make relative
+ * @param folder The workspace folder
+ */
 export function unresolveFilePath(filePath: string, folder: WorkspaceFolder): string {
     // tslint:disable-next-line: no-invalid-template-strings
     let replacedPath = filePath.replace(folder.uri.fsPath, '${workspaceFolder}');
