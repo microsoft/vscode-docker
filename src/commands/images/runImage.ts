1f033c24bf5dadab4b6133f02ceb5064b338392e
xgdgsc <xgdgsc@users.noreply.github.com>
2019-09-16 16:15:38 +0800

    :bug: rm rm

--- src/commands/images/runImage.ts
+++ src/commands/images/runImage.ts
@@ -23,7 +23,7 @@ async function runImageCore(context: IActionContext, node: ImageTreeItem | undef
     const inspectInfo = await node.getImage().inspect();
     const ports: string[] = inspectInfo.Config.ExposedPorts ? Object.keys(inspectInfo.Config.ExposedPorts) : [];
 
-    let options = `--rm ${interactive ? '-it' : '-d'}`;
+    let options = `${interactive ? '-it' : '-d'}`;
     if (ports.length) {
         const portMappings = ports.map((port) => `-p ${port.split("/")[0]}:${port}`); //'port' is of the form number/protocol, eg. 8080/udp.
         // In the command, the host port has just the number (mentioned in the EXPOSE step), while the destination port can specify the protocol too
