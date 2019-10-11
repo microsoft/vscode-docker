5f6d6b0892663ace30f867641389cd305044ab30
Brandon Waterloo <36966225+bwateratmsft@users.noreply.github.com>
2019-10-11 14:51:48 -0400

    Term fix

--- src/tree/registries/RegistriesTreeItem.ts
+++ src/tree/registries/RegistriesTreeItem.ts
@@ -98,8 +98,8 @@ export class RegistriesTreeItem extends AzExtParentTreeItem {
             context.telemetry.properties.cancelStep = 'learnHowToContribute';
             throw new UserCancelledError();
         } else if (provider.onlyOneAllowed && this._cachedProviders.find(c => c.id === provider.id)) {
-            await ext.ui.showWarningMessage(`The "${provider.id}" registry is already connected.`);
-            context.telemetry.properties.cancelStep = 'registryAlreadyAdded';
+            await ext.ui.showWarningMessage(`The "${provider.id}" registry provider is already connected.`);
+            context.telemetry.properties.cancelStep = 'registryProviderAlreadyAdded';
             throw new UserCancelledError();
         }
 
