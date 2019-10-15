305a406b5b67f5bbefa13b29e21800d3fb027c52
Brandon Waterloo <36966225+bwateratmsft@users.noreply.github.com>
2019-10-15 13:22:00 -0400

    Upload hourly

--- src/registerListeners.ts
+++ src/registerListeners.ts
@@ -6,14 +6,21 @@
 import { ExtensionContext, TextDocument, workspace } from 'vscode';
 import { ext } from './extensionVariables';
 
+let lastUploadTime: number = 0;
+const hourInMilliseconds = 60 * 60 * 1000;
+
 export function registerListeners(ctx: ExtensionContext): void {
     ctx.subscriptions.push(
-        workspace.onDidSaveTextDocument((doc: TextDocument) => {
-            if (doc.languageId !== 'dockerfile') {
-                return;
-            }
+        workspace.onDidSaveTextDocument(
+            (doc: TextDocument) => {
+                // If it's not a Dockerfile, or last upload time is within an hour, skip
+                if (doc.languageId !== 'dockerfile' || lastUploadTime + hourInMilliseconds > Date.now()) {
+                    return;
+                }
 
-            ext.reporter.sendTelemetryEvent('dockerfilesave', {}, { "lineCount": doc.lineCount });
-        })
+                lastUploadTime = Date.now();
+                ext.reporter.sendTelemetryEvent('dockerfilesave', { "lineCount": doc.lineCount.toString() }, {});
+            }
+        )
     );
 }
