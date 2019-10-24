f3a3e9f75ab6c09065ef7d6e3f97bbcf7c2a81cb
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2019-10-24 14:20:36 -0400

    Cherry pick several things for 0.8.2 release (#1367)
    * Update package
    
    * Cherry pick clearer error message for duplicate registry
    
    * Cherry pick improved Dockerfile pattern matches, save event telemetry
    
    * Cherry pick only refresh if VSCode is in focus
    
    * Cherry pick add a command to create the simplest network
    
    * Cherry pick ask some active users to take NPS survey

---
+++ src/registerListeners.ts
@@ -0,0 +1,24 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ExtensionContext, TextDocument, workspace } from 'vscode';
+import { ext } from './extensionVariables';
+
+let lastUploadTime: number = 0;
+const hourInMilliseconds = 1000 * 60 * 60;
+
+export function registerListeners(ctx: ExtensionContext): void {
+    ctx.subscriptions.push(workspace.onDidSaveTextDocument(onDidSaveTextDocument));
+}
+
+function onDidSaveTextDocument(doc: TextDocument): void {
+    // If it's not a Dockerfile, or last upload time is within an hour, skip
+    if (doc.languageId !== 'dockerfile' || lastUploadTime + hourInMilliseconds > Date.now()) {
+        return;
+    }
+
+    lastUploadTime = Date.now();
+    ext.reporter.sendTelemetryEvent('dockerfilesave', { "lineCount": doc.lineCount.toString() }, {});
+}
