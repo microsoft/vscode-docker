28c6007dbad15137f1fe4d02872ce6499bccc628
Brandon Waterloo <36966225+bwateratmsft@users.noreply.github.com>
2019-11-13 11:02:52 -0600

    Quoting args

--- src/tasks/netcore/updateBlazorManifest.ts
+++ src/tasks/netcore/updateBlazorManifest.ts
@@ -41,7 +41,7 @@ export async function updateBlazorManifest(context: DockerRunTaskContext, runDef
 
     const targetsFile = path.join(ext.context.asAbsolutePath('resources'), 'GetBlazorManifestLocations.targets');
 
-    const command = `dotnet build /r:false /t:GetBlazorManifestLocations /p:CustomAfterMicrosoftCommonTargets=${targetsFile} /p:BlazorManifestLocationsOutput=${locationsFile} ${runDefinition.netCore.appProject}`;
+    const command = `dotnet build /r:false /t:GetBlazorManifestLocations /p:CustomAfterMicrosoftCommonTargets="${targetsFile}" /p:BlazorManifestLocationsOutput="${locationsFile}" "${runDefinition.netCore.appProject}"`;
 
     try {
         await execAsync(command, { timeout: 5000 });
