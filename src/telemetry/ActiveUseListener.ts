7a1f2371561691d398c2c05f074eef14e3676f0a
Phillip Hoff <phillip@orst.edu>
2020-02-06 11:56:13 -0800

    Update global state with last event dates.

--- src/telemetry/ActiveUseListener.ts
+++ src/telemetry/ActiveUseListener.ts
@@ -6,20 +6,36 @@
 import * as vscode from 'vscode';
 import { ITelemetryPublisher } from './TelemetryPublisher';
 
+const lastActivationDateKey = 'ActiveUseListener.lastActivationDate';
+const lastUseDateKey = 'ActiveUseListener.lastUseDate';
+
 export default class ActiveUseListener extends vscode.Disposable {
     private readonly listener: vscode.Disposable;
+
+    private lastActivationDate: string | undefined;
+    private lastUseDate: string | undefined;
 
     public constructor(publisher: ITelemetryPublisher, state: vscode.Memento) {
         super(() => { this.listener.dispose(); });
 
         this.listener = publisher.onEvent(
             event => {
-                if (event?.properties?.isActivationEvent === 'true') {
-                    // Update last activation date.
-                    console.log('Saw activation event!');
+                const eventDate = new Date().toDateString();
+
+                if (event.properties?.isActivationEvent === 'true') {
+                    if (this.lastActivationDate !== eventDate) {
+                        this.lastActivationDate = eventDate;
+
+                        // NOTE: Ignore failures to update (as we'll try again next time).
+                        state.update(lastActivationDateKey, this.lastActivationDate).then(() => {}, () => {});
+                    }
                 } else {
-                    // Update last use date.
-                    console.log('Saw real use!');
+                    if (this.lastUseDate !== eventDate) {
+                        this.lastUseDate = eventDate;
+
+                        // NOTE: Ignore failures to update (as we'll try again next time).
+                        state.update(lastUseDateKey, this.lastUseDate).then(() => {}, () => {});
+                    }
                 }
             });
     }
