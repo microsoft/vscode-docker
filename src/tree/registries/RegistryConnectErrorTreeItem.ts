72cac94a8fe1fbf4e20cd5ef9c503c425f4ad18e
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-02-19 16:50:45 -0500

    Refactoring to remove a parameter

--- src/tree/registries/RegistryConnectErrorTreeItem.ts
+++ src/tree/registries/RegistryConnectErrorTreeItem.ts
@@ -5,15 +5,20 @@
 
 import { AzExtParentTreeItem, GenericTreeItem, parseError } from "vscode-azureextensionui";
 import { getThemedIconPath } from "../IconPath";
+import { getRegistryProviders } from "./all/getRegistryProviders";
 import { ICachedRegistryProvider } from "./ICachedRegistryProvider";
 import { IRegistryProvider } from "./IRegistryProvider";
 
 export class RegistryConnectErrorTreeItem extends GenericTreeItem {
-    public constructor(parent: AzExtParentTreeItem, err: unknown, public readonly cachedProvider: ICachedRegistryProvider, public readonly provider: IRegistryProvider) {
+    public constructor(parent: AzExtParentTreeItem, err: unknown, public readonly cachedProvider: ICachedRegistryProvider) {
         super(parent, {
             label: parseError(err).message,
             contextValue: 'registryConnectError',
             iconPath: getThemedIconPath('statusWarning')
         });
+
+        this.provider = getRegistryProviders().find(rp => rp.id === this.cachedProvider.id);
     }
+
+    public readonly provider: IRegistryProvider;
 }
