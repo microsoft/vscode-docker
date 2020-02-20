2bcd19247e489490e4e1eba51b96edde1ac2e37e
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-02-20 12:48:57 -0500

    Volumes commands

--- src/commands/volumes/pruneVolumes.ts
+++ src/commands/volumes/pruneVolumes.ts
@@ -6,20 +6,21 @@
 import { window } from 'vscode';
 import { IActionContext } from 'vscode-azureextensionui';
 import { ext } from '../../extensionVariables';
+import { localize } from "../../localize";
 import { callDockerodeWithErrorHandling } from '../../utils/callDockerodeWithErrorHandling';
 import { convertToMB } from '../../utils/convertToMB';
 
 export async function pruneVolumes(context: IActionContext): Promise<void> {
-    const confirmPrune: string = "Are you sure you want to remove all unused volumes?";
+    const confirmPrune: string = localize('vscode-docker.commands.volumes.prune.confirm', 'Are you sure you want to remove all unused volumes?');
     // no need to check result - cancel will throw a UserCancelledError
-    await ext.ui.showWarningMessage(confirmPrune, { modal: true }, { title: 'Remove' });
+    await ext.ui.showWarningMessage(confirmPrune, { modal: true }, { title: localize('vscode-docker.commands.volumes.prune.remove', 'Remove') });
 
     /* eslint-disable-next-line @typescript-eslint/promise-function-async */
     const result = await callDockerodeWithErrorHandling(() => ext.dockerode.pruneVolumes(), context);
 
     const numDeleted = (result.VolumesDeleted || []).length;
     const mbReclaimed = convertToMB(result.SpaceReclaimed);
-    let message = `Removed ${numDeleted} volumes(s) and reclaimed ${mbReclaimed}MB of space.`;
+    let message = localize('vscode-docker.commands.volumes.prune.removed', 'Removed {0} volumes(s) and reclaimed {1}MB of space.', numDeleted, mbReclaimed);
     // don't wait
     /* eslint-disable-next-line @typescript-eslint/no-floating-promises */
     window.showInformationMessage(message);
