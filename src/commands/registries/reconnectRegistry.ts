eca5cbcf273c1fe2949ad3e5fcf3a968ad8ba9b6
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-02-27 13:15:00 -0500

    Loc catchup through commands folder

--- src/commands/registries/reconnectRegistry.ts
+++ src/commands/registries/reconnectRegistry.ts
@@ -5,12 +5,13 @@
 
 import { IActionContext } from "vscode-azureextensionui";
 import { ext } from "../../extensionVariables";
+import { localize } from '../../localize';
 import { RegistryConnectErrorTreeItem } from "../../tree/registries/RegistryConnectErrorTreeItem";
 
 export async function reconnectRegistry(context: IActionContext, node?: RegistryConnectErrorTreeItem): Promise<void> {
     if (!node?.cachedProvider || !node?.provider) {
         // This is not expected ever, so we'll throw an error which can be bubbled up to a Report Issue if it does
-        throw new Error('Unable to determine provider to re-enter credentials. Please disconnect and connect again.');
+        throw new Error(localize('vscode-docker.commands.registries.reconnectError', 'Unable to determine provider to re-enter credentials. Please disconnect and connect again.'));
     }
 
     await ext.registriesRoot.disconnectRegistry(context, node.cachedProvider);
