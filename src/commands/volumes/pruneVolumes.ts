cb637a691cc29791b9e8266487e4d5356c583090
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-03-02 16:15:06 -0500

    Wrap prune commands in progress toast

--- src/commands/volumes/pruneVolumes.ts
+++ src/commands/volumes/pruneVolumes.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { window } from 'vscode';
+import * as vscode from 'vscode';
 import { IActionContext } from 'vscode-azureextensionui';
 import { ext } from '../../extensionVariables';
 import { localize } from "../../localize";
@@ -15,13 +15,18 @@ export async function pruneVolumes(context: IActionContext): Promise<void> {
     // no need to check result - cancel will throw a UserCancelledError
     await ext.ui.showWarningMessage(confirmPrune, { modal: true }, { title: localize('vscode-docker.commands.volumes.prune.remove', 'Remove') });
 
-    /* eslint-disable-next-line @typescript-eslint/promise-function-async */
-    const result = await callDockerodeWithErrorHandling(() => ext.dockerode.pruneVolumes(), context);
+    await vscode.window.withProgress(
+        { location: vscode.ProgressLocation.Notification, title: localize('vscode-docker.commands.volumes.pruning', 'Pruning volumes...') },
+        async () => {
+            /* eslint-disable-next-line @typescript-eslint/promise-function-async */
+            const result = await callDockerodeWithErrorHandling(() => ext.dockerode.pruneVolumes(), context);
 
-    const numDeleted = (result.VolumesDeleted || []).length;
-    const mbReclaimed = convertToMB(result.SpaceReclaimed);
-    let message = localize('vscode-docker.commands.volumes.prune.removed', 'Removed {0} volumes(s) and reclaimed {1}MB of space.', numDeleted, mbReclaimed);
-    // don't wait
-    /* eslint-disable-next-line @typescript-eslint/no-floating-promises */
-    window.showInformationMessage(message);
+            const numDeleted = (result.VolumesDeleted || []).length;
+            const mbReclaimed = convertToMB(result.SpaceReclaimed);
+            let message = localize('vscode-docker.commands.volumes.prune.removed', 'Removed {0} volumes(s) and reclaimed {1}MB of space.', numDeleted, mbReclaimed);
+            // don't wait
+            /* eslint-disable-next-line @typescript-eslint/no-floating-promises */
+            vscode.window.showInformationMessage(message);
+        }
+    );
 }
