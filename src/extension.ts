f16ebe34a5edf27e703d55cfa9c4e949ffcdd9f5
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-04-10 13:56:58 -0400

    Disable active use survey

--- src/extension.ts
+++ src/extension.ts
@@ -23,7 +23,6 @@ import { ext } from './extensionVariables';
 import { localize } from './localize';
 import { registerListeners } from './registerListeners';
 import { registerTaskProviders } from './tasks/TaskHelper';
-import { registerActiveUseSurvey } from './telemetry/surveys/activeUseSurvey';
 import { TelemetryPublisher } from './telemetry/TelemetryPublisher';
 import { TelemetryReporterProxy } from './telemetry/TelemetryReporterProxy';
 import { registerTrees } from './tree/registerTrees';
@@ -63,7 +62,8 @@ function initializeExtensionVariables(ctx: vscode.ExtensionContext): void {
     const publisher = new TelemetryPublisher();
     ctx.subscriptions.push(publisher);
 
-    ctx.subscriptions.push(registerActiveUseSurvey(publisher, ctx.globalState));
+    // Temporarily disabled
+    // ctx.subscriptions.push(registerActiveUseSurvey(publisher, ctx.globalState));
 
     ext.reporter = new TelemetryReporterProxy(publisher, createTelemetryReporter(ctx));
     if (!ext.keytar) {
