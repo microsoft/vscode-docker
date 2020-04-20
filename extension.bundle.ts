d0b342d63ca28f43d8fd42a917b4e98089dbf91e
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-04-20 15:22:09 -0400

    Add an async lazy class and lazy lifetimes (#1861)
    * Add an async lazy class and lazy lifetimes
    
    * Add unit tests

--- extension.bundle.ts
+++ extension.bundle.ts
@@ -28,6 +28,8 @@ export { DotNetClient } from './src/debugging/coreclr/CommandLineDotNetClient';
 export { compareBuildImageOptions, LaunchOptions } from './src/debugging/coreclr/dockerManager';
 export { FileSystemProvider } from './src/debugging/coreclr/fsProvider';
 export { LineSplitter } from './src/debugging/coreclr/lineSplitter';
+export { delay } from './src/utils/delay';
+export { Lazy, AsyncLazy } from './src/utils/lazy';
 export { OSProvider } from './src/utils/LocalOSProvider';
 export { DockerDaemonIsLinuxPrerequisite, DockerfileExistsPrerequisite, DotNetSdkInstalledPrerequisite, LinuxUserInDockerGroupPrerequisite, MacNuGetFallbackFolderSharedPrerequisite } from './src/debugging/coreclr/prereqManager';
 export { ext } from './src/extensionVariables';
--- src/utils/lazy.ts
+++ src/utils/lazy.ts
@@ -7,7 +7,7 @@ export class Lazy<T> {
     private _isValueCreated: boolean = false;
     private _value: T | undefined;
 
-    public constructor(private readonly valueFactory: () => T) {
+    public constructor(private readonly valueFactory: () => T, private readonly _valueLifetime?: number) {
     }
 
     public get isValueCreated(): boolean {
@@ -15,13 +15,69 @@ export class Lazy<T> {
     }
 
     public get value(): T {
-        if (!this._isValueCreated) {
-            this._value = this.valueFactory();
-            this._isValueCreated = true;
+        if (this._isValueCreated) {
+            return this._value;
+        }
+
+        this._value = this.valueFactory();
+        this._isValueCreated = true;
+
+        if (this._valueLifetime) {
+            const reset = setTimeout(() => {
+                this._isValueCreated = false;
+                this._value = undefined;
+                clearTimeout(reset);
+            }, this._valueLifetime);
         }
 
         return this._value;
     }
 }
 
-export default Lazy;
+export class AsyncLazy<T> {
+    private _isValueCreated: boolean = false;
+    private _value: T | undefined;
+    private _valuePromise: Promise<T> | undefined;
+
+    public constructor(private readonly valueFactory: () => Promise<T>, private readonly _valueLifetime?: number) {
+    }
+
+    public get isValueCreated(): boolean {
+        return this._isValueCreated;
+    }
+
+    public async getValue(): Promise<T> {
+        if (this._isValueCreated) {
+            return this._value;
+        }
+
+        const isPrimaryPromise = this._valuePromise === undefined; // The first caller is "primary"
+
+        if (isPrimaryPromise) {
+            this._valuePromise = this.valueFactory();
+        }
+
+        const result = await this._valuePromise;
+
+        if (isPrimaryPromise) {
+            this._value = result;
+            this._valuePromise = undefined;
+            this._isValueCreated = true;
+        }
+
+        if (this._valueLifetime && isPrimaryPromise) {
+            const reset = setTimeout(() => {
+                // Will only clear out values if there isn't a currently-running Promise
+                // If there is, this timer will skip, but when that Promise finishes it will go through this code and register a new timer
+                if (this._valuePromise === undefined) {
+                    this._isValueCreated = false;
+                    this._value = undefined;
+                }
+
+                clearTimeout(reset);
+            }, this._valueLifetime);
+        }
+
+        return result;
+    }
+}
