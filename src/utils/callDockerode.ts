a38ea53200f68460a57e4a3d08c1dccf270ea30a
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-04-21 15:31:30 -0400

    Cancel pending Dockerode calls upon context switch

--- src/utils/callDockerode.ts
+++ src/utils/callDockerode.ts
@@ -5,11 +5,15 @@
 
 /* eslint-disable unicorn/filename-case */
 
-import { IActionContext, parseError } from "vscode-azureextensionui";
+import * as vscode from 'vscode';
+import { IActionContext, parseError, UserCancelledError } from 'vscode-azureextensionui';
 import { ext } from '../extensionVariables';
 import { localize } from '../localize';
 import { dockerContextManager } from './dockerContextManager';
 import { refreshDockerode } from './refreshDockerode';
+
+let cts: vscode.CancellationTokenSource | undefined;
+let cancellationPromise: Promise<never> | undefined;
 
 export async function callDockerode<T>(dockerodeCallback: () => T): Promise<T> {
     const p = new Promise<T>((resolve, reject) => {
@@ -22,18 +26,6 @@ export async function callDockerode<T>(dockerodeCallback: () => T): Promise<T> {
     });
 
     return callDockerodeAsync(async () => p);
-}
-
-export async function callDockerodeAsync<T>(dockerodeAsyncCallback: () => Promise<T>): Promise<T> {
-    // If running tests, don't refresh Dockerode (some tests override Dockerode)
-    if (!ext.runningTests) {
-        const { Changed: contextChanged } = await dockerContextManager.getCurrentContext();
-        if (contextChanged) {
-            await refreshDockerode();
-        }
-    }
-
-    return await dockerodeAsyncCallback();
 }
 
 export async function callDockerodeWithErrorHandling<T>(dockerodeCallback: () => Promise<T>, context: IActionContext): Promise<T> {
@@ -50,4 +42,37 @@ export async function callDockerodeWithErrorHandling<T>(dockerodeCallback: () =>
 
         throw err;
     }
+}
+
+export async function callDockerodeAsync<T>(dockerodeAsyncCallback: () => Promise<T>): Promise<T> {
+    // If running tests, don't refresh Dockerode (some tests override Dockerode)
+    if (!ext.runningTests) {
+        const { Changed: contextChanged } = await dockerContextManager.getCurrentContext();
+        if (contextChanged) {
+            cts.cancel(); // This will cause any still-awaiting promises to reject with UserCancelledError
+            cts.dispose();
+            cts = cancellationPromise = undefined;
+
+            await refreshDockerode();
+        }
+    }
+
+    return await Promise.race([dockerodeAsyncCallback(), getCancellationPromise()]);
+}
+
+async function getCancellationPromise(): Promise<never> {
+    if (cancellationPromise) {
+        return cancellationPromise;
+    }
+
+    if (!cts) {
+        cts = new vscode.CancellationTokenSource();
+    }
+
+    return cancellationPromise = new Promise((resolve, reject) => {
+        const disposable = cts.token.onCancellationRequested(() => {
+            disposable.dispose();
+            reject(new UserCancelledError());
+        });
+    });
 }
