03a6360883ce3d609dd63438d8babd2bd88bf77a
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-05-01 10:34:18 -0400

    Use registerEvent for save listener

--- src/registerListeners.ts
+++ src/registerListeners.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { ExtensionContext, TextDocument, workspace } from 'vscode';
+import { IActionContext, registerEvent } from 'vscode-azureextensionui';
 import { ext } from './extensionVariables';
 
 let lastUploadTime: number = 0;
@@ -11,19 +12,18 @@ const hourInMilliseconds = 1000 * 60 * 60;
 
 export function registerListeners(ctx: ExtensionContext): void {
     if (ext.telemetryOptIn) {
-        ctx.subscriptions.push(workspace.onDidSaveTextDocument(onDidSaveTextDocument));
+        registerEvent('dockerfilesave', workspace.onDidSaveTextDocument, async (context: IActionContext, doc: TextDocument) => {
+            // If it's not a Dockerfile, or last upload time is within an hour, skip
+            if (doc.languageId !== 'dockerfile' || lastUploadTime + hourInMilliseconds > Date.now()) {
+                context.telemetry.suppressAll = true;
+                return;
+            }
+
+            lastUploadTime = Date.now();
+            context.telemetry.properties.lineCount = doc.lineCount.toString();
+
+            // eslint-disable-next-line no-unused-expressions, @typescript-eslint/no-floating-promises
+            ext.activityMeasurementService.recordActivity('overall');
+        });
     }
-}
-
-function onDidSaveTextDocument(doc: TextDocument): void {
-    // If it's not a Dockerfile, or last upload time is within an hour, skip
-    if (doc.languageId !== 'dockerfile' || lastUploadTime + hourInMilliseconds > Date.now()) {
-        return;
-    }
-
-    lastUploadTime = Date.now();
-    ext.reporter.sendTelemetryErrorEvent('dockerfilesave', { "lineCount": doc.lineCount.toString() }, {});
-
-    // eslint-disable-next-line no-unused-expressions, @typescript-eslint/no-floating-promises
-    ext.activityMeasurementService.recordActivity('overall');
 }
