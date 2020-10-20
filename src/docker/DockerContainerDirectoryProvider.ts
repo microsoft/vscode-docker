586bc5da034a9dce00936b79da08c2d6518adc50
Phillip Hoff <phillip@orst.edu>
2020-10-20 15:42:36 -0700

    Sketch listing Windows directories.

--- src/docker/DockerContainerDirectoryProvider.ts
+++ src/docker/DockerContainerDirectoryProvider.ts
@@ -63,6 +63,60 @@ function parseLinuxDirectoryItems(input: string, parentPath: string): DirectoryI
     return items;
 }
 
+function parseWindowsName(dirOrSize: string, name: string): string {
+    const symlinkPlaceholder = '<SYMLINKD>';
+
+    if (dirOrSize === symlinkPlaceholder) {
+        const expression = /(?<name>.+)(?= \[)/g;
+        const match = expression.exec(name);
+
+        if (match !== null) {
+            return match.groups.name;
+        }
+    }
+
+    return name;
+}
+
+function parseWindowsType(dirOrSize: string): DirectoryItemType {
+    const size = Number.parseInt(dirOrSize, 10);
+
+    return Number.isNaN(size) ? 'directory' : 'file';
+}
+
+function parseWindowsDirectoryItems(input: string, parentPath: string): DirectoryItem[] {
+    const expression = /^(?<date>\d{1,2}\/\d{1,2}\/\d{4})\s+(?<time>\d{1,2}:\d{1,2}( (AM|PM))?)\s+(?<dirOrSize><DIR>|<SYMLINKD>|\d+)\s+(?<name>.*)$/gm;
+
+    let match = expression.exec(input);
+
+    const items: DirectoryItem[] = [];
+
+    while (match !== null) {
+        const name = parseWindowsName(match.groups.dirOrSize, match.groups.name);
+        const type = parseWindowsType(match.groups.dirOrSize);
+
+        match = expression.exec(input);
+
+        //
+        // NOTE: Do not use `match` below this point.
+        //
+
+        // Ignore relative directory items...
+        if (type === 'directory' && (name === '.' || name === '..')) {
+            continue;
+        }
+
+        items.push(
+            {
+                name,
+                path: path.posix.join(parentPath, name),
+                type
+            });
+    }
+
+    return items;
+}
+
 async function getLinuxContainerDirectoryItems(executor: DockerContainerExecutor, containerId: string, parentPath: string | undefined): Promise<DirectoryItem[]> {
     if (parentPath === undefined) {
         parentPath = '/';
@@ -76,18 +130,16 @@ async function getLinuxContainerDirectoryItems(executor: DockerContainerExecutor
 }
 
 async function getWindowsContainerDirectoryItems(executor: DockerContainerExecutor, containerId: string, parentPath: string | undefined): Promise<DirectoryItem[]> {
-    return [
-        {
-            name: 'dir',
-            path: '/dir',
-            type: 'directory'
-        },
-        {
-            name: 'file.txt',
-            path: '/file.txt',
-            type: 'file'
-        }
-    ];
+    if (parentPath.startsWith('/')) {
+        parentPath = path.win32.join('C:\\', parentPath.substr(1));
+    }
+
+    // TODO: Try different users on error.
+    const command = `cmd /C dir /A-S /-C "${parentPath}"`;
+
+    const output = await executor(containerId, command);
+
+    return parseWindowsDirectoryItems(output, parentPath);
 }
 
 export async function getContainerDirectoryItems(executor: DockerContainerExecutor, containerId: string, parentPath: string | undefined, osType: DockerOSType): Promise<DirectoryItem[]> {
