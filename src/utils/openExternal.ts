add5094d54854a7469bf6f978d9daf344e6b999f
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-10-26 11:03:16 -0400

    Links in help pane

--- src/utils/openExternal.ts
+++ src/utils/openExternal.ts
@@ -3,14 +3,8 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { commands, Uri } from 'vscode';
-import { localize } from '../localize';
+import * as vscode from 'vscode';
 
 export async function openExternal(path: string, throwErrorOnFailure: boolean = false): Promise<void> {
-    let uri = Uri.parse(path);
-    let successful: boolean = await commands.executeCommand('vscode.open', uri);
-    if (!successful && throwErrorOnFailure) {
-        throw new Error(localize('vscode-docker.utils.openExternal.error', 'Opening {0} was unsuccessful', path));
-    }
-    return;
+    await vscode.env.openExternal(vscode.Uri.parse(path));
 }
