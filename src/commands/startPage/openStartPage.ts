6055c70efd9725da16be68b0b298a3850bbd6aeb
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-10-27 10:10:22 -0400

    Feedback

--- src/commands/startPage/openStartPage.ts
+++ src/commands/startPage/openStartPage.ts
@@ -17,18 +17,14 @@ export async function openStartPage(context: IActionContext, reason: 'install' |
     await startPage.createOrShow(context);
 }
 
-export async function openStartPageIfNecessary(): Promise<void> {
-    const lastVersion = new semver.SemVer(ext.context.globalState.get(lastVersionKey, '0.0.1'));
-    const thisVersion = new semver.SemVer(extensionVersion.value);
-    const diff = semver.diff(thisVersion, lastVersion);
-
+export async function openStartPageAfterExtensionUpdate(): Promise<void> {
     if (!vscode.workspace.getConfiguration('docker').get('showStartPage', false)) {
         // Don't show: disabled by settings
         return;
     } else if (!/^en(-us)?$/i.test(vscode.env.language)) {
         // Don't show: this page is English only
         return;
-    } else if (semver.lte(thisVersion, lastVersion) || diff === 'prepatch' || diff === 'patch' || diff === 'prerelease') {
+    } else if (!isHigherMinorVersion(extensionVersion.value, ext.context.globalState.get(lastVersionKey, '0.0.1'))) {
         // Don't show: already showed during this major/minor
         return;
     } else if (!(await ext.experimentationService.isLiveFlightEnabled('vscode-docker.openStartPage'))) {
@@ -39,4 +35,33 @@ export async function openStartPageIfNecessary(): Promise<void> {
     // Show!
     await ext.context.globalState.update(lastVersionKey, extensionVersion.value);
     void vscode.commands.executeCommand('vscode-docker.help.openStartPage', 'install');
+}
+
+// Exported just for unit tests
+export function isHigherMinorVersion(a: string | semver.SemVer, b: string | semver.SemVer): boolean {
+    if (typeof (a) === 'string') {
+        a = semver.coerce(a);
+    }
+
+    if (typeof (b) === 'string') {
+        b = semver.coerce(b);
+    }
+
+    const diff = semver.diff(a, b);
+
+    // If a is less than or equal to b, it's automatically not a higher minor version
+    if (semver.lte(a, b)) {
+        return false;
+    }
+
+    // Otherwise compare; if different majors or minors then true, else false
+    switch (diff) {
+        case 'premajor':
+        case 'major':
+        case 'preminor':
+        case 'minor':
+            return true;
+        default:
+            return false;
+    }
 }
