c2813cd8c73d1b1f7d00794e5613dce905c2c4eb
Phillip Hoff <phillip@orst.edu>
2020-11-02 14:45:34 -0800

    Rename file (phase I).

--- src/commands/registerCommands.ts
+++ src/commands/registerCommands.ts
@@ -14,7 +14,7 @@ import { attachShellContainer } from "./containers/attachShellContainer";
 import { browseContainer } from "./containers/browseContainer";
 import { composeGroupDown, composeGroupRestart } from "./containers/composeGroup";
 import { configureContainersExplorer } from "./containers/configureContainersExplorer";
-import { openContainerFile } from "./containers/files/openContainerFIle";
+import { openContainerFile } from "./containers/files/openContainerFile2";
 import { inspectContainer } from "./containers/inspectContainer";
 import { pruneContainers } from "./containers/pruneContainers";
 import { removeContainer } from "./containers/removeContainer";
