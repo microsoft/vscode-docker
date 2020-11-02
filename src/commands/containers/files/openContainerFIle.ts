2ca4568fef79c8c91432be45e45b6dd1a1ac0791
Phillip Hoff <phillip@orst.edu>
2020-11-02 09:41:40 -0800

    Add copyright headers.

--- src/commands/containers/files/openContainerFIle.ts
+++ src/commands/containers/files/openContainerFIle.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as vscode from 'vscode';
 import { IActionContext } from 'vscode-azureextensionui';
 import { ext } from '../../../extensionVariables';
