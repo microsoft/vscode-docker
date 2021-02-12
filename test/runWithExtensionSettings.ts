a25c13cc383cb6a53cebeeb094da16dbd8687c15
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-02-12 14:54:47 -0500

    Cut down the test time a bunch so it stops timing out

---
+++ test/runWithExtensionSettings.ts
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ConfigurationTarget, workspace, WorkspaceConfiguration } from "vscode";
+import { configPrefix } from "../extension.bundle";
+
+export async function runWithExtensionSettings<TCallback>(newValues: { [key: string]: any }, callback: () => Promise<TCallback>): Promise<TCallback> {
+    const config: WorkspaceConfiguration = workspace.getConfiguration(configPrefix);
+
+    const oldValues: { [key: string]: any } = {};
+
+    for (const key of Object.keys(newValues)) {
+        oldValues[key] = config.inspect(key)?.globalValue;
+    }
+
+    try {
+        for (const key of Object.keys(newValues)) {
+            oldValues[key] = config.inspect(key)?.globalValue;
+            await config.update(key, newValues[key], ConfigurationTarget.Global);
+        }
+
+        return await callback();
+    } finally {
+        for (const key of Object.keys(oldValues)) {
+            await config.update(key, oldValues[key], ConfigurationTarget.Global);
+        }
+    }
+}
