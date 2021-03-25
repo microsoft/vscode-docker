dadafd2229313c2847e5449f167b9f608bc4003f
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-03-25 11:26:28 -0400

    Redo webpacking

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,16 @@
 
 // See https://github.com/Microsoft/vscode-azuretools/wiki/webpack for guidance
 
-'use strict';
+// 'use strict';
 
 /* eslint-disable @typescript-eslint/no-var-requires */
-const process = require('process');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const StringReplacePlugin = require('string-replace-webpack-plugin');
-const dev = require('vscode-azureextensiondev');
+// const process = require('process');
+// const webpack = require('webpack');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const StringReplacePlugin = require('string-replace-webpack-plugin');
+// const dev = require('vscode-azureextensiondev');
 /* eslint-enable @typescript-eslint/no-var-requires */
-
+/*
 let DEBUG_WEBPACK = !!process.env.DEBUG_WEBPACK;
 
 let config = dev.getDefaultWebpackConfig({
@@ -114,4 +114,48 @@ if (DEBUG_WEBPACK) {
     console.log('Config:', config);
 }
 
+module.exports = config;
+*/
+
+// @ts-check
+
+'use strict';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+/** @type {import('webpack').Configuration}*/
+const config = {
+    target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+
+    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: {
+        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2'
+    },
+    devtool: 'nosources-source-map',
+    externals: {
+        vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    },
+    resolve: {
+        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
 module.exports = config;
