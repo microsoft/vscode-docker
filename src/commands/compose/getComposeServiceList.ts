cf475ab8b48056c97e8c01760dcfdaef705b82ef
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-03-30 11:51:23 -0400

    Commit current progress

--- src/commands/compose/getComposeServiceList.ts
+++ src/commands/compose/getComposeServiceList.ts
@@ -12,17 +12,39 @@ import { execAsync } from '../../utils/spawnAsync';
 // Matches an `up` or `down` and everything after it--so that it can be replaced with `config --services`, to get a service list using all of the files originally part of the compose command
 const composeCommandReplaceRegex = /(\b(up|down)\b).*$/i;
 
+type SubsetType = 'services' | 'profiles';
+
 export async function getComposeServiceList(context: IActionContext, workspaceFolder: vscode.WorkspaceFolder, composeCommand: string): Promise<string> {
-    const services = await getServices(workspaceFolder, composeCommand);
+    const services = await getServiceSubsets(workspaceFolder, composeCommand, 'services');
+    const profiles = await getServiceSubsets(workspaceFolder, composeCommand, 'profiles');
+
+    let useProfiles = false;
+    // If there any profiles, we need to ask the user whether they want profiles or services, since they are mutually exclusive to use
+    if (profiles?.length) {
+        const profilesOrServices: IAzureQuickPickItem<SubsetType>[] = [
+            {
+                label: localize('vscode-docker.getComposeServiceList.services', 'Services'),
+                data: 'services'
+            },
+            {
+                label: localize('vscode-docker.getComposeServiceList.profiles', 'Profiles'),
+                data: 'profiles'
+            }
+        ];
+
+        useProfiles = 'profiles' === (await ext.ui.showQuickPick(profilesOrServices, { placeHolder: localize('vscode-docker.getComposeServiceList.servicesOrProfiles', 'Do you want to start services or profiles?') })).data;
+    }
 
     // Fetch the previously chosen services list. By default, all will be selected.
     const workspaceServiceListKey = `vscode-docker.composeServices.${workspaceFolder.name}`;
-    const previousChoices = ext.context.workspaceState.get<string[]>(workspaceServiceListKey, services);
+    const workspaceProfileListKey = `vscode-docker.composeProfiles.${workspaceFolder.name}`;
+    const previousServiceChoices = ext.context.workspaceState.get<string[]>(workspaceServiceListKey, services);
+    const previousProfileChoices = ext.context.workspaceState.get<string[] | undefined>(workspaceProfileListKey, profiles);
 
     const pickChoices: IAzureQuickPickItem<string>[] = services.map(s => ({
         label: s,
         data: s,
-        picked: previousChoices.some(p => p === s),
+        picked: previousServiceChoices.some(p => p === s),
     }));
 
     const subsetChoices =
@@ -36,6 +58,7 @@ export async function getComposeServiceList(context: IActionContext, workspaceFo
 
     context.telemetry.measurements.totalServices = pickChoices.length;
     context.telemetry.measurements.chosenServices = subsetChoices.length;
+    context.telemetry.properties.subsetType = useProfiles ? 'profiles' : 'services';
 
     // Update the cache
     await ext.context.workspaceState.update(workspaceServiceListKey, subsetChoices.map(c => c.data));
@@ -43,13 +66,22 @@ export async function getComposeServiceList(context: IActionContext, workspaceFo
     return subsetChoices.map(c => c.data).join(' ');
 }
 
-async function getServices(workspaceFolder: vscode.WorkspaceFolder, composeCommand: string): Promise<string[]> {
-    // Start by getting a new command with the exact same files list (replaces the "up ..." or "down ..." with "config --services")
-    const configCommand = composeCommand.replace(composeCommandReplaceRegex, 'config --services');
+async function getServiceSubsets(workspaceFolder: vscode.WorkspaceFolder, composeCommand: string, type: SubsetType): Promise<string[] | undefined> {
+    try {
+        // Start by getting a new command with the exact same files list (replaces the "up ..." or "down ..." with "config --services" or "config --profiles")
+        const configCommand = composeCommand.replace(composeCommandReplaceRegex, `config --${type}`);
 
-    const { stdout } = await execAsync(configCommand, { cwd: workspaceFolder.uri.fsPath });
+        const { stdout } = await execAsync(configCommand, { cwd: workspaceFolder.uri.fsPath });
 
-    // The output of the config command is a list of services, one per line
-    // Split them up and remove empty entries
-    return stdout.split(/\r?\n/im).filter(l => { return l; });
+        // The output of the config command is a list of services / profiles, one per line
+        // Split them up and remove empty entries
+        return stdout.split(/\r?\n/im).filter(l => { return l; });
+    } catch (err) {
+        // Profiles is not yet widely supported, so those errors will be eaten--otherwise, rethrow
+        if (type === 'profiles') {
+            return undefined;
+        } else {
+            throw err;
+        }
+    }
 }
