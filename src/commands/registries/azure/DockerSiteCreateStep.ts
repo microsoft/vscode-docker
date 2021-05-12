57d779fcb40e6bce9b94b5e954331d33e8773732
Ravikumar Palanisamy <ravipal@microsoft.com>
2021-05-12 11:27:46 -0700

    Deploy container image to Azure Arc (#2924)
    * Deploy container image to Azure Arc
    
    * Lazy load CustomLocationListStep

--- src/commands/registries/azure/DockerSiteCreateStep.ts
+++ src/commands/registries/azure/DockerSiteCreateStep.ts
@@ -4,17 +4,21 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { WebSiteManagementClient, WebSiteManagementModels } from '@azure/arm-appservice'; // These are only dev-time imports so don't need to be lazy
+import { Site } from '@azure/arm-appservice/esm/models'; // These are only dev-time imports so don't need to be lazy
 import { Progress } from "vscode";
 import { IAppServiceWizardContext } from "vscode-azureappservice"; // These are only dev-time imports so don't need to be lazy
-import { AzureWizardExecuteStep, createAzureClient } from "vscode-azureextensionui";
+import { AzExtLocation, AzureWizardExecuteStep, createAzureClient, LocationListStep } from "vscode-azureextensionui";
 import { ext } from "../../../extensionVariables";
 import { localize } from "../../../localize";
+import { AzureRegistryTreeItem } from '../../../tree/registries/azure/AzureRegistryTreeItem';
+import { RegistryTreeItemBase } from '../../../tree/registries/RegistryTreeItemBase';
+import { RemoteTagTreeItem } from '../../../tree/registries/RemoteTagTreeItem';
 import { nonNullProp, nonNullValueAndProp } from "../../../utils/nonNull";
 
 export class DockerSiteCreateStep extends AzureWizardExecuteStep<IAppServiceWizardContext> {
     public priority: number = 140;
 
-    public constructor(private readonly siteConfig: WebSiteManagementModels.SiteConfig) {
+    public constructor(private readonly siteConfig: WebSiteManagementModels.SiteConfig, private readonly node: RemoteTagTreeItem) {
         super();
     }
 
@@ -22,19 +26,56 @@ export class DockerSiteCreateStep extends AzureWizardExecuteStep<IAppServiceWiza
         const creatingNewApp: string = localize('vscode-docker.commands.registries.azure.deployImage.creatingWebApp', 'Creating web app "{0}"...', context.newSiteName);
         ext.outputChannel.appendLine(creatingNewApp);
         progress.report({ message: creatingNewApp });
+        const siteConfig = await this.getSiteConfig(context);
+        const location: AzExtLocation = await LocationListStep.getLocation(context);
+        const locationName: string = nonNullProp(location, 'name');
 
         const armAppService = await import('@azure/arm-appservice');
         const client: WebSiteManagementClient = createAzureClient(context, armAppService.WebSiteManagementClient);
-        context.site = await client.webApps.createOrUpdate(nonNullValueAndProp(context.resourceGroup, 'name'), nonNullProp(context, 'newSiteName'), {
+        const siteEnvelope: Site = {
             name: context.newSiteName,
-            kind: 'app,linux',
-            location: nonNullValueAndProp(context.location, 'name'),
+            location: locationName,
             serverFarmId: nonNullValueAndProp(context.plan, 'id'),
-            siteConfig: this.siteConfig,
-            identity: {
+            siteConfig: siteConfig
+        };
+
+        if (context.customLocation) {
+            // deploying to Azure Arc
+            siteEnvelope.kind = 'app,linux,kubernetes,container';
+        }
+        else {
+            siteEnvelope.identity = {
                 type: 'SystemAssigned'
-            },
-        });
+            };
+        }
+
+        context.site = await client.webApps.createOrUpdate(nonNullValueAndProp(context.resourceGroup, 'name'), nonNullProp(context, 'newSiteName'), siteEnvelope);
+    }
+
+    private async getSiteConfig(context: IAppServiceWizardContext): Promise<WebSiteManagementModels.SiteConfig> {
+        // Temporary workaround until Arc adds support for managed identity, so use usename and password instead.
+        // When customLocation is set, then user is deploying to Arc.
+        const registryTreeItem: RegistryTreeItemBase = this.node.parent.parent;
+        if (registryTreeItem instanceof AzureRegistryTreeItem && context.customLocation) {
+            const appSettings: WebSiteManagementModels.NameValuePair[] = [];
+            const cred = await registryTreeItem.tryGetAdminCredentials();
+            if (!cred) {
+                throw new Error(localize('vscode-docker.commands.registries.azure.dockersitecreatestep.notAdminEnabled', 'Azure App service deployment on Azure Arc only supports running images from Azure Container Registries with admin enabled'));
+            } else {
+                appSettings.push({ name: "DOCKER_REGISTRY_SERVER_URL", value: registryTreeItem.baseUrl });
+                appSettings.push({ name: "DOCKER_REGISTRY_SERVER_USERNAME", value: cred.username });
+                appSettings.push({ name: "DOCKER_REGISTRY_SERVER_PASSWORD", value: nonNullProp(cred, 'passwords')[0].value });
+                appSettings.push({ name: "DOCKER_ENABLE_CI", value: 'true' });
+                const linuxFxVersion = `DOCKER|${registryTreeItem.baseImagePath}/${this.node.repoNameAndTag}`;
+                return {
+                    linuxFxVersion,
+                    appSettings
+                };
+            }
+        }
+        else {
+            return this.siteConfig;
+        }
     }
 
     public shouldExecute(context: IAppServiceWizardContext): boolean {
