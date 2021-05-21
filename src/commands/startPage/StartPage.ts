0dfc5e5f565a7bad53fe7aaa0fd04fbe6f692a19
Karol Zadora-Przylecki <karolz@microsoft.com>
2021-05-21 11:35:29 -0700

    Address code review feedback

--- src/commands/startPage/StartPage.ts
+++ src/commands/startPage/StartPage.ts
@@ -31,10 +31,6 @@ class StartPage {
     public async createOrShow(context: IActionContext): Promise<void> {
         const resourcesRoot = vscode.Uri.joinPath(ext.context.extensionUri, 'resources');
 
-        // If we're using the bundled version, the codicons root URI is at <extensionRoot>/dist/node_modules/vscode-codicons/dist
-        // If we're not using the bundled version, the codicons root URI is <extensionRoot>/node_modules/vscode-codicons/dist
-        const codiconsRoot = vscode.Uri.joinPath(ext.context.extensionUri, ...ext.ignoreBundle ? ['node_modules'] : ['dist', 'node_modules'], 'vscode-codicons', 'dist');
-
         if (!this.activePanel) {
             const template = await this.getTemplate(resourcesRoot);
 
@@ -45,41 +41,52 @@ class StartPage {
                 // Best effort
             }
 
-            this.activePanel = vscode.window.createWebviewPanel(
-                'vscode-docker.startPage',
-                localize('vscode-docker.help.startPage.title', 'Docker - Get Started'),
-                vscode.ViewColumn.One,
-                {
-                    enableCommandUris: true,
-                    enableScripts: true,
-                    localResourceRoots: [resourcesRoot, codiconsRoot],
-                }
-            );
-
-            const listener = this.activePanel.webview.onDidReceiveMessage(async (message: WebviewMessage) => this.handleMessage(message));
-
-            this.activePanel.onDidDispose(() => {
-                this.activePanel = undefined;
-                listener.dispose();
-            });
-
-            const webview = this.activePanel.webview;
-
-            const startPageContext: StartPageContext = {
-                cspSource: webview.cspSource,
-                nonce: cryptoUtils.getRandomHexString(8),
-                codiconsFontUri: webview.asWebviewUri(vscode.Uri.joinPath(codiconsRoot, 'codicon.ttf')).toString(),
-                codiconsStyleUri: webview.asWebviewUri(vscode.Uri.joinPath(codiconsRoot, 'codicon.css')).toString(),
-                dockerIconUri: webview.asWebviewUri(vscode.Uri.joinPath(resourcesRoot, 'docker_blue.png')).toString(),
-                showStartPageChecked: vscode.workspace.getConfiguration('docker').get('showStartPage', false) ? 'checked' : '',
-                isMac: isMac(),
-                showWhatsNew: showWhatsNew,
-            };
-
-            this.activePanel.webview.html = template(startPageContext);
+            // createOrShow() might have been called multiple times in short timeframe
+            if (!this.activePanel) {
+                this.doCreatePanel(resourcesRoot, showWhatsNew, template);
+            }
         }
 
         this.activePanel.reveal();
+    }
+
+    private doCreatePanel(resourcesRoot: vscode.Uri, showWhatsNew: boolean, template: HandlebarsTemplateDelegate<unknown>) {
+        // If we're using the bundled version, the codicons root URI is at <extensionRoot>/dist/node_modules/vscode-codicons/dist
+        // If we're not using the bundled version, the codicons root URI is <extensionRoot>/node_modules/vscode-codicons/dist
+        const codiconsRoot = vscode.Uri.joinPath(ext.context.extensionUri, ...ext.ignoreBundle ? ['node_modules'] : ['dist', 'node_modules'], 'vscode-codicons', 'dist');
+
+        this.activePanel = vscode.window.createWebviewPanel(
+            'vscode-docker.startPage',
+            localize('vscode-docker.help.startPage.title', 'Docker - Get Started'),
+            vscode.ViewColumn.One,
+            {
+                enableCommandUris: true,
+                enableScripts: true,
+                localResourceRoots: [resourcesRoot, codiconsRoot],
+            }
+        );
+
+        const listener = this.activePanel.webview.onDidReceiveMessage(async (message: WebviewMessage) => this.handleMessage(message));
+
+        this.activePanel.onDidDispose(() => {
+            this.activePanel = undefined;
+            listener.dispose();
+        });
+
+        const webview = this.activePanel.webview;
+
+        const startPageContext: StartPageContext = {
+            cspSource: webview.cspSource,
+            nonce: cryptoUtils.getRandomHexString(8),
+            codiconsFontUri: webview.asWebviewUri(vscode.Uri.joinPath(codiconsRoot, 'codicon.ttf')).toString(),
+            codiconsStyleUri: webview.asWebviewUri(vscode.Uri.joinPath(codiconsRoot, 'codicon.css')).toString(),
+            dockerIconUri: webview.asWebviewUri(vscode.Uri.joinPath(resourcesRoot, 'docker_blue.png')).toString(),
+            showStartPageChecked: vscode.workspace.getConfiguration('docker').get('showStartPage', false) ? 'checked' : '',
+            isMac: isMac(),
+            showWhatsNew: showWhatsNew,
+        };
+
+        this.activePanel.webview.html = template(startPageContext);
     }
 
     private async handleMessage(message: WebviewMessage): Promise<void> {
