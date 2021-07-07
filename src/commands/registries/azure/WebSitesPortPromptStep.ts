27932ba82436b01112ef416a2d630dd64706ca0b
Ravikumar Palanisamy <ravipal@microsoft.com>
2021-07-07 15:03:26 -0700

    Always require port for custom location

--- src/commands/registries/azure/WebSitesPortPromptStep.ts
+++ src/commands/registries/azure/WebSitesPortPromptStep.ts
@@ -11,9 +11,11 @@ import { IAppServiceContainerWizardContext } from './deployImageToAzure';
 export class WebSitesPortPromptStep extends AzureWizardPromptStep<IAppServiceContainerWizardContext> {
 
     public async prompt(context: IAppServiceContainerWizardContext): Promise<void> {
-        const prompt: string = localize('vscode-docker.deployAppService.WebSitesPortPromptStep.whatPort', 'What port does your app listen on? Leave it empty for app listening on port 80.');
-        const portString: string = await ext.ui.showInputBox({ prompt, validateInput });
-        context.webSitesPort = parseInt(portString, 10) || null;
+        const prompt: string = localize('vscode-docker.deployAppService.WebSitesPortPromptStep.whatPort', 'What port does your app listen on?');
+        const placeHolder: string = '80';
+        const value: string = '80';
+        const portString: string = await ext.ui.showInputBox({ prompt, placeHolder, value, validateInput });
+        context.webSitesPort = parseInt(portString, 10);
     }
 
     public shouldPrompt(context: IAppServiceContainerWizardContext): boolean {
@@ -22,9 +24,6 @@ export class WebSitesPortPromptStep extends AzureWizardPromptStep<IAppServiceCon
 }
 
 function validateInput(value: string | undefined): string | undefined {
-    if (!value || value === '') {
-        return undefined;
-    }
     if (Number(value)) {
         const port: number = parseInt(value, 10);
         if (port >= 1 && port <= 65535) {
@@ -32,6 +31,6 @@ function validateInput(value: string | undefined): string | undefined {
         }
     }
 
-    return localize('vscode-docker.deployAppService.WebSitesPortPromptStep.InvalidPort', 'Port must be a positive integer (1 to 65535), or empty for container listening on port 80.');
+    return localize('vscode-docker.deployAppService.WebSitesPortPromptStep.InvalidPort', 'Port must be a positive integer (1 to 65535).');
 }
 
