cf36c103451cba847d2454a4c21ececdf4449508
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-08-05 11:21:00 -0400

    Add support for cancelling file open (#3118)

--- src/commands/containers/files/openContainerFile.ts
+++ src/commands/containers/files/openContainerFile.ts
@@ -9,6 +9,7 @@ import { ext } from '../../../extensionVariables';
 import { localize } from '../../../localize';
 import { FileTreeItem } from '../../../tree/containers/files/FileTreeItem';
 import { multiSelectNodes } from '../../../utils/multiSelectNodes';
+import { getCancelPromise } from '../../../utils/promiseUtils';
 
 export async function openContainerFile(context: IActionContext, node?: FileTreeItem, nodes?: FileTreeItem[]): Promise<void> {
     nodes = await multiSelectNodes(
@@ -22,18 +23,26 @@ export async function openContainerFile(context: IActionContext, node?: FileTree
     await vscode.window.withProgress(
         {
             location: vscode.ProgressLocation.Notification,
-            title: localize('vscode-docker.commands.containers.files.openContainerFile.opening', 'Opening File(s)...')
+            title: localize('vscode-docker.commands.containers.files.openContainerFile.opening', 'Opening File(s)...'),
+            cancellable: true
         },
-        async () => {
-            await Promise.all(
+        async (task, token) => {
+            const filesPromise = Promise.all(
                 nodes.map(
                     async n => {
                         const document = await vscode.workspace.openTextDocument(n.uri.uri);
 
+                        if (token.isCancellationRequested) {
+                            return;
+                        }
+
                         // NOTE: If only a single file is opened, use "preview mode" which replaces the document with next opened (the default for opening files in VS Code).
                         //       If multiple files are opened, do not use preview mode so that each document will be shown (otherwise only the last would only be shown).
-
                         await vscode.window.showTextDocument(document, { preview: nodes.length === 1 });
-                    }));
+                    }
+                )
+            );
+
+            await Promise.race([filesPromise, getCancelPromise(token)]);
         });
 }
