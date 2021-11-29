7f2eef7c61909aa9d4f09714033384f07457a6f4
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-11-29 13:39:01 -0500

    Change show-dangling to more UX-friendly toggle

--- src/commands/images/showDanglingImages.ts
+++ src/commands/images/showDanglingImages.ts
@@ -3,10 +3,16 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as vscode from 'vscode';
 import { ext } from '../../extensionVariables';
 import { IActionContext } from 'vscode-azureextensionui';
 
 export async function showDanglingImages(context: IActionContext): Promise<void> {
-    const conf: boolean = ext.context.globalState.get('vscode-docker.images.showDanglingImages', false);
-    await ext.context.globalState.update('vscode-docker.images.showDanglingImages', !conf);
+    await vscode.commands.executeCommand('setContext', 'vscode-docker:danglingShown', true);
+    await ext.context.globalState.update('vscode-docker.images.showDanglingImages', true);
+}
+
+export async function hideDanglingImages(context: IActionContext): Promise<void> {
+    await vscode.commands.executeCommand('setContext', 'vscode-docker:danglingShown', false);
+    await ext.context.globalState.update('vscode-docker.images.showDanglingImages', false);
 }
