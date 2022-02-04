53952faf13e25f5ad57ea16504a7f914b87f76ca
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-02-04 09:54:55 -0500

    Commit current progress

---
+++ src/telemetry/surveys/awareness2.ts
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ext } from '../../extensionVariables';
+import { localize } from '../../localize';
+import { Survey } from './SurveyManager';
+
+const minimumOverallSessions = 2;
+const maximumNotEditOnlySessions = 0;
+
+export const awareness2: Survey = {
+    id: 'aware2',
+    prompt: localize('vscode-docker.survey.aware.prompt', 'The Docker extension is always adding new features. Would you like to learn more about them?'),
+    buttons: new Map<string, string | undefined>([
+        [localize('vscode-docker.survey.aware.button.watch', 'Watch a video'), 'https://aka.ms/AA8lh3s'],
+        [localize('vscode-docker.survey.aware.button.read', 'Read docs'), 'https://aka.ms/AA9j59w'],
+        [localize('vscode-docker.survey.aware.button.never', 'Don\'t show again'), undefined],
+    ]),
+    activationDelayMs: 30 * 1000,
+    isEligible: isEligible,
+};
+
+async function isEligible(): Promise<boolean> {
+    const overallActivity = ext.activityMeasurementService.getActivityMeasurement('overall');
+    const noEditActivity = ext.activityMeasurementService.getActivityMeasurement('overallnoedit');
+
+    return overallActivity.totalSessions >= minimumOverallSessions &&
+        noEditActivity.totalSessions <= maximumNotEditOnlySessions;
+}
