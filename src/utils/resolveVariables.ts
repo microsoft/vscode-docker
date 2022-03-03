9e9363d9d3093d8cb5e56719399ee3abd9b642ed
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-03-03 14:39:47 -0500

    Support `userHome` variable

--- src/utils/resolveVariables.ts
+++ src/utils/resolveVariables.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as os from 'os';
 import * as path from 'path';
 import { WorkspaceFolder, window, workspace } from 'vscode';
 import { cloneObject } from '../utils/cloneObject';
@@ -41,6 +42,8 @@ function resolveSingleVariable(variable: string, folder?: WorkspaceFolder, addit
             case '${workspaceFolder}':
             case '${workspaceRoot}':
                 return path.normalize(folder.uri.fsPath);
+            case '${userHome}':
+                return os.homedir();
             case '${relativeFile}':
                 return path.relative(path.normalize(folder.uri.fsPath), getActiveFilePath());
             default:
