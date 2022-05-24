1119a704e1ac26bd56ddebd661acb2230956ccff
IIFelix <56734789+IIFelix@users.noreply.github.com>
2022-05-24 21:50:43 +0200

    Fix 'Add Docker Files to Workspace' command

--- src/tasks/python/PythonTaskHelper.ts
+++ src/tasks/python/PythonTaskHelper.ts
@@ -69,7 +69,13 @@ export class PythonTaskHelper implements TaskHelper {
             runOptions.module = 'flask';
             runOptions.file = undefined;
         } else if (options.projectType === 'fastapi') {
-            runOptions.args.unshift(`${path.basename(runOptions.file, '.py')}:app`);
+            const basename = path.basename(runOptions.file, '.py');
+            const dirname = path.dirname(runOptions.file).replace(path.sep, '.');
+            if (dirname !== '.') {
+              runOptions.args.unshift(`${dirname}.${basename}:app`);
+            } else {
+              runOptions.args.unshift(`${basename}:app`);
+            }
             runOptions.module = 'uvicorn';
             runOptions.file = undefined;
         }
