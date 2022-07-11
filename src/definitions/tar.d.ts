d22952b5d05a92a95cd94a0a67c4b8477eb29985
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-07-11 12:10:39 -0400

    Add tar utils

--- src/definitions/tar.d.ts
+++ src/definitions/tar.d.ts
@@ -3,23 +3,58 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+/**
+ * The `@types/tar` package is riddled with inaccuracies, so it's easier
+ * to just declare typings for it here
+ */
 declare module "tar" {
-    export interface ParseClass {
-        // eslint-disable-next-line @typescript-eslint/prefer-function-type
-        new(): NodeJS.ReadWriteStream;
+    //#region Parse
+
+    export interface ParseOptions {
+        filter?: (path: string, entry: ReadEntryClass) => boolean;
+        onentry?: (entry: ReadEntryClass) => void;
+    }
+
+    export interface ParseClass extends NodeJS.ReadWriteStream {
+        // eslint-disable-next-line @typescript-eslint/no-misused-new
+        new(options?: ParseOptions): ParseClass;
     }
 
     export const Parse: ParseClass;
 
-    export interface ReadEntry extends NodeJS.EventEmitter {
+    //#endregion Parse
+
+    //#region Pack
+
+    export interface PackOptions {
+        portable?: boolean;
+    }
+
+    export interface PackClass extends NodeJS.ReadWriteStream {
+        // eslint-disable-next-line @typescript-eslint/no-misused-new
+        new(options?: PackOptions): PackClass;
+        add(readEntry: ReadEntryClass): void;
+    }
+
+    export const Pack: PackClass;
+
+    //#endregion Pack
+
+    //#region ReadEntry
+
+    export interface ReadEntryOptions {
+        path: string;
+        type: 'File' | 'Directory';
+        size: number;
+    }
+
+    export interface ReadEntryClass extends NodeJS.EventEmitter, NodeJS.ReadWriteStream {
+        // eslint-disable-next-line @typescript-eslint/no-misused-new
+        new(options: ReadEntryOptions): ReadEntryClass;
         path: string;
     }
 
-    export interface CreateOptions {
-        cwd?: string;
-    }
+    export const ReadEntry: ReadEntryClass;
 
-    export function create(options: CreateOptions, fileList: string[]): NodeJS.ReadableStream;
-
-    export const c: typeof create;
+    //#endregion ReadEntry
 }
