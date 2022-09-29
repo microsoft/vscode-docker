1d08102d492eeafd683c5e5a40aee9688d6336a5
Bruno Alassia <vruno.alassia@gmail.com>
2022-09-28 22:20:25 -0700

    show progress window

--- src/commands/images/scanImage.ts
+++ src/commands/images/scanImage.ts
@@ -14,6 +14,32 @@ import * as fs from 'fs/promises';
 import * as path from 'path';
 import * as os from 'os';
 
+async function scan(context: IActionContext, node?: ImageTreeItem): Promise<any> {
+    // creating temp folder to save results
+    const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'atomist-scan'));
+    const sbomFile = 'sbom.json';
+    const cmd = [
+        ext.dockerContextManager.getDockerCommand(context),
+        'run',
+        '-v', `${tmpDir}:/atm`, // mount folder to be able to read the result
+        `-v`, `"/var/run/docker.sock":"/var/run/docker.sock"`, // sharing docker socket
+        'ghcr.io/cdupuis/index-cli-plugin:main',
+        'index', 'sbom', '--image', node.fullTag, '--output', `/atm/${sbomFile}`, '--include-vulns'
+    ];
+    await executeAsTask(
+        context,
+        cmd.join(' '),
+        `Scanning ${node.fullTag}`,
+        { addDockerEnv: true, focus: true }
+    );
+
+    const sbomPath = `${tmpDir}/${sbomFile}`;
+    // reading and parsing results
+    const rawResults = await fs.readFile(sbomPath, { encoding: 'utf8', flag: 'r' });
+    const sbomResults = JSON.parse(rawResults + '');
+    return sbomResults;
+}
+
 export async function scanImageWithAtomist(context: IActionContext, node?: ImageTreeItem): Promise<void> {
     if (!node) {
         await ext.imagesTree.refresh(context);
@@ -22,15 +48,10 @@ export async function scanImageWithAtomist(context: IActionContext, node?: Image
             noItemFoundErrorMessage: localize('vscode-docker.commands.images.inspect.noImages', 'No images are available to scan')
         });
     }
-
-    const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'atomist-scan'));
-    const sbomFile = 'sbom.json';
-    await executeAsTask(
-        context,
-        `${ext.dockerContextManager.getDockerCommand(context)} run -it -v ${tmpDir}:/atm -v "/var/run/docker.sock":"/var/run/docker.sock" ghcr.io/cdupuis/index-cli-plugin:main index sbom --image ${node.fullTag} --output /atm/${sbomFile} --include-vulns`, 'Scanning', { addDockerEnv: true, focus: true },
-    );
-    const sbomPath = `${tmpDir}/${sbomFile}`;
-    const rawResults = await fs.readFile(sbomPath, { encoding: 'utf8', flag: 'r' });
-    const sbomResults = JSON.parse(rawResults + '');
+    const progressOptions: vscode.ProgressOptions = {
+        location: vscode.ProgressLocation.Notification,
+        title: localize('vscode-docker.commands.images.scan', 'Scanning for vulnerabilities...'),
+    };
+    const sbomResults = await vscode.window.withProgress(progressOptions, async () => await scan(context, node));
     CVEWebViewPanel.show(vscode.Uri.parse("./"), sbomResults?.vulnerabilities, node.fullTag);
 }
