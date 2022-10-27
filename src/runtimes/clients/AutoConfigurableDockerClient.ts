a532eac6d8f81349ef4367c9c185be97f39841c3
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-10-27 16:08:50 -0400

    Autodetect compose configuration (#3669)

---
+++ src/runtimes/clients/AutoConfigurableDockerClient.ts
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { DockerClient } from '../docker';
+import { AutoConfigurableClient } from './AutoConfigurableClient';
+
+export class AutoConfigurableDockerClient extends DockerClient implements AutoConfigurableClient {
+    public constructor() {
+        super();
+        this.reconfigure();
+    }
+
+    public reconfigure(): void {
+        const config = vscode.workspace.getConfiguration('docker');
+        const dockerCommand = config.get<string | undefined>('dockerPath') || 'docker';
+        this.commandName = dockerCommand;
+    }
+}
