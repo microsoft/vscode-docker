d0c00768aa085b695cdf71db3b39a0fc8741b7ee
Oliver King <kingoliver@microsoft.com>
2022-11-10 16:45:48 -0500

    address feedback

--- src/commands/registries/azure/tasks/buildImageInAzure.ts
+++ src/commands/registries/azure/tasks/buildImageInAzure.ts
@@ -5,24 +5,29 @@
 
 import * as vscode from 'vscode';
 import { IActionContext, UserCancelledError } from '@microsoft/vscode-azext-utils';
-import { KnownRunStatus, Run } from '@azure/arm-containerregistry';
+import type { Run } from '@azure/arm-containerregistry';
 import { scheduleRunRequest } from './scheduleRunRequest';
-import { sleep } from '../../../../utils/sleep';
+import { delay } from '../../../../utils/promiseUtils';
+import { getArmContainerRegistry } from '../../../../utils/lazyPackages';
 
-const WAIT_MS = 500;
+const WAIT_MS = 5000;
 
-export async function buildImageInAzure(context: IActionContext, uri?: vscode.Uri | undefined): Promise<Run> {
+export async function buildImageInAzure(context: IActionContext, uri?: vscode.Uri | undefined): Promise<Run | undefined> {
     if (!vscode.workspace.isTrusted) {
         throw new UserCancelledError('enforceTrust');
     }
 
     const getRun = await scheduleRunRequest(context, "DockerBuildRequest", uri);
+    
     let run = await getRun();
+    const { KnownRunStatus }= await getArmContainerRegistry();
     while (run.status === KnownRunStatus.Running) {
-        await sleep(WAIT_MS);
+        await delay(WAIT_MS);
         run = await getRun();
     }
 
+    // we are returning the run so that other extensions can consume this with the vscode.commands.executeCommand
+    // currently it is used by the ms-kubernetes-tools.aks-devx-tools extension (https://github.com/Azure/aks-devx-tools)
     return run;
 }
 
--- src/utils/sleep.ts
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
