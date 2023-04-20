25f3b4ed759884c681f50320f94e0f78defafac2
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2023-04-20 12:25:45 -0400

    Final updates to track2

--- src/utils/lazyPackages.ts
+++ src/utils/lazyPackages.ts
@@ -5,8 +5,9 @@
 
 import { ext } from '../extensionVariables';
 
-export async function getArmAuth(): Promise<typeof import('@azure/arm-authorization')> {
-    return await import('@azure/arm-authorization');
+// Switch back to `@azure/arm-authorization` when https://github.com/Azure/azure-sdk-for-js/issues/21210 is fixed
+export async function getArmAuth(): Promise<typeof import('@azure/arm-authorization-profile-2020-09-01-hybrid')> {
+    return await import('@azure/arm-authorization-profile-2020-09-01-hybrid');
 }
 
 export async function getArmContainerRegistry(): Promise<typeof import('@azure/arm-containerregistry')> {
