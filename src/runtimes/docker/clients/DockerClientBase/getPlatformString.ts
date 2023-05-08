215eecbe9f3d936cdb4e88f8c721c3b5b1904fa5
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-05-08 11:09:12 -0400

    added stricter type cheking

--- src/runtimes/docker/clients/DockerClientBase/getPlatformString.ts
+++ src/runtimes/docker/clients/DockerClientBase/getPlatformString.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { l10n } from "vscode";
 import { BuildImageCommandOptions } from "../../contracts/ContainerClient";
 
 /**
@@ -10,12 +11,22 @@ import { BuildImageCommandOptions } from "../../contracts/ContainerClient";
  * @returns Platform string specified by `platform`
  */
 export function getPlatformString(options: BuildImageCommandOptions): string {
-    const platform = options.platform;
+    const platform = options?.platform;
+
+    if (!platform) {
+        throw new Error(l10n.t("Platform is not specified."));
+    }
+
     if (typeof platform === "string") {
         return platform;
     } else {
         const os = platform.os ?? "";
         const architecture = platform.architecture ?? "";
+
+        if (!os || !architecture) {
+            throw new Error(l10n.t("Platform is missing `os` or `architecture` or both properties."));
+        }
+
         return `${os}/${architecture}`;
     }
 }
