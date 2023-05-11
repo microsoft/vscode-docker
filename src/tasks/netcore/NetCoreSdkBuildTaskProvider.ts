127f053d85af52a069b5d22955766155c14c0d61
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-05-11 17:50:41 -0400

    added build options for DotnetSdk

--- src/tasks/netcore/NetCoreSdkBuildTaskProvider.ts
+++ src/tasks/netcore/NetCoreSdkBuildTaskProvider.ts
@@ -3,21 +3,13 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { IActionContext } from "@microsoft/vscode-azext-utils";
-import { CancellationToken, Task, WorkspaceFolder } from "vscode";
-import { composeArgs, withArg, withNamedArg } from "../../runtimes/docker";
+import { Task } from "vscode";
+import { composeArgs, getNativeArchitecture, withArg, withNamedArg } from "../../runtimes/docker";
 import { cloneObject } from "../../utils/cloneObject";
-import { DockerPseudoterminal } from "../DockerPseudoterminal";
+import { getDockerOSType } from "../../utils/osUtils";
 import { DockerTaskProvider } from "../DockerTaskProvider";
-import { throwIfCancellationRequested } from "../TaskHelper";
-import { NetCoreSdkBuildDefinitionBase } from "./NetCoreSdkBuildTaskDefinitionBase";
-
-export interface NetCoreSdkBuildContext {
-    folder: WorkspaceFolder;
-    actionContext?: IActionContext;
-    cancellationToken?: CancellationToken;
-    terminal: DockerPseudoterminal;
-}
+import { DockerTaskContext, DockerTaskExecutionContext, getDefaultImageName, throwIfCancellationRequested } from "../TaskHelper";
+import { NetCoreSdkBuildDefinitionBase, NetCoreSdkBuildOptions } from "./NetCoreSdkBuildTaskDefinitionBase";
 
 export interface NetCoreSdkBuildTask extends Task {
     definition: NetCoreSdkBuildDefinitionBase;
@@ -25,23 +17,19 @@ export interface NetCoreSdkBuildTask extends Task {
 
 export class NetCoreSdkBuildProvider extends DockerTaskProvider {
 
-    public constructor() {
-        super('dotnet-sdk-build',);
-    }
+    public constructor() { super('dotnet-sdk-build', undefined); }
 
-    protected async executeTaskInternal(context: NetCoreSdkBuildContext, task: NetCoreSdkBuildTask): Promise<void> {
+    protected async executeTaskInternal(context: DockerTaskExecutionContext, task: Task): Promise<void> {
         const buildDefinition = cloneObject(task.definition);
-        buildDefinition.netcoreSdkBuild = buildDefinition.netcoreSdkBuild || {};
-
-        // TODO: check to see if netcoreSdkBuild is defined, if not apply defaults to netcoreSdkBuild object through `getNetcoreSdkBuildOptions()`
+        buildDefinition.netcoreSdkBuild = await this.getDotnetSdkBuildOptions(context, buildDefinition);
 
         throwIfCancellationRequested(context);
 
         const sdkBuildCommand = composeArgs(
             withArg('dotnet', 'publish'),
-            withNamedArg('-os', buildDefinition.netcoreSdkBuild?.os),
-            withNamedArg('-arch', buildDefinition.netcoreSdkBuild?.arcitecture),
-            withNamedArg('-c', buildDefinition.netcoreSdkBuild?.configuration),
+            withNamedArg('-os', buildDefinition.netcoreSdkBuild.platform?.os),
+            withNamedArg('-arch', buildDefinition.netcoreSdkBuild.platform?.architecture),
+            withNamedArg('-c', buildDefinition.netcoreSdkBuild.configuration),
         )();
 
         const commandLine = sdkBuildCommand.join(' ');
@@ -54,7 +42,20 @@ export class NetCoreSdkBuildProvider extends DockerTaskProvider {
         );
 
         throwIfCancellationRequested(context);
-
     }
 
+    public async getDotnetSdkBuildOptions(context: DockerTaskContext, buildDefinition: NetCoreSdkBuildDefinitionBase): Promise<NetCoreSdkBuildOptions> {
+
+        const buildOptions = buildDefinition.netcoreSdkBuild || {};
+
+        buildOptions.platform = {
+            architecture: buildOptions.platform?.architecture || getNativeArchitecture(),
+            os: buildOptions.platform?.os || await getDockerOSType()
+        };
+
+        buildOptions.configuration = 'Debug'; // intentionally default to Debug configuration for now
+        buildOptions.tag = buildOptions.tag || getDefaultImageName(context.folder.name);
+
+        return buildOptions;
+    }
 }
