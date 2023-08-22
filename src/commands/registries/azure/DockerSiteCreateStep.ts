f0003c173c6df5eb042557a3a3cf95b0c82ffb64
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-08-22 17:17:08 -0400

    fix small mistake

--- src/commands/registries/azure/DockerSiteCreateStep.ts
+++ src/commands/registries/azure/DockerSiteCreateStep.ts
@@ -7,7 +7,7 @@ import type { NameValuePair, Site, SiteConfig, WebSiteManagementClient } from '@
 import type { CustomLocation } from "@microsoft/vscode-azext-azureappservice"; // These are only dev-time imports so don't need to be lazy
 import type { AzExtLocation } from '@microsoft/vscode-azext-azureutils'; // These are only dev-time imports so don't need to be lazy
 import { AzureWizardExecuteStep, nonNullProp, nonNullValueAndProp } from "@microsoft/vscode-azext-utils";
-import { CommonRegistry, CommonTag, isDockerHubRegistryItem, isGenericV2RegistryItem } from '@microsoft/vscode-docker-registries';
+import { CommonRegistry, CommonTag, isDockerHubRegistryItem, isGenericV2Registry } from '@microsoft/vscode-docker-registries';
 import { Progress, l10n } from "vscode";
 import { ext } from "../../../extensionVariables";
 import { AzureRegistryDataProvider, isAzureRegistryItem } from '../../../tree/registries/Azure/AzureRegistryDataProvider';
@@ -89,16 +89,18 @@ export class DockerSiteCreateStep extends AzureWizardExecuteStep<IAppServiceCont
             registryUrl = registryTI.wrappedItem.baseUrl.toString();
         }
         // Docker Hub -> App Service *OR* Arc App Service
-        else if (isDockerHubRegistryItem(registryTI)) {
+        else if (isDockerHubRegistryItem(registryTI.wrappedItem)) {
             const loginInformation = await registryTI.provider.getLoginInformation(registryTI.wrappedItem);
             username = loginInformation.username;
             password = await loginInformation.secret;
             registryUrl = 'https://index.docker.io';
         }
         // Generic registry -> App Service *OR* Arc App Service
-        else if (isGenericV2RegistryItem(registryTI)) {
-            registryUrl = registryTI.baseUrl.toString();
-        } else {
+        else if (isGenericV2Registry(registryTI.wrappedItem)) {
+            registryUrl = (registryTI.wrappedItem as CommonRegistry).baseUrl.toString();
+        }
+        // TODO: add case for GitHub Container Registry
+        else {
             throw new RangeError(l10n.t('Unrecognized node type "{0}"', registryTI.constructor.name));
         }
 
