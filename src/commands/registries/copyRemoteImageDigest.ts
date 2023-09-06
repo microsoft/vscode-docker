62ff37adb0c454f5c478f029d32146c5d2bc1e89
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-09-06 13:57:14 -0400

    minor changes to imports

--- src/commands/registries/copyRemoteImageDigest.ts
+++ src/commands/registries/copyRemoteImageDigest.ts
@@ -4,18 +4,18 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IActionContext, contextValueExperience } from "@microsoft/vscode-azext-utils";
-import { CommonTag, RegistryV2DataProvider, V2Tag } from "@microsoft/vscode-docker-registries";
+import { RegistryV2DataProvider, V2Tag } from "@microsoft/vscode-docker-registries";
 import * as vscode from "vscode";
 import { ext } from "../../extensionVariables";
 import { UnifiedRegistryItem } from "../../tree/registries/UnifiedRegistryTreeDataProvider";
 
-export async function copyRemoteImageDigest(context: IActionContext, node?: UnifiedRegistryItem<CommonTag>): Promise<void> {
+export async function copyRemoteImageDigest(context: IActionContext, node?: UnifiedRegistryItem<V2Tag>): Promise<void> {
     if (!node) {
         node = await contextValueExperience(context, ext.registriesTree, { include: ['registryV2Tag'] });
     }
 
     const v2DataProvider = node.provider as unknown as RegistryV2DataProvider;
-    const digest = await v2DataProvider.getImageDigest(node.wrappedItem as V2Tag);
+    const digest = await v2DataProvider.getImageDigest(node.wrappedItem);
 
     /* eslint-disable-next-line @typescript-eslint/no-floating-promises */
     vscode.env.clipboard.writeText(digest);
