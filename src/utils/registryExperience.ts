1ae2c3a75ab34d953dd7665b66bc338f445bbfeb
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-09-14 16:35:12 -0400

    added comments

--- src/utils/registryExperience.ts
+++ src/utils/registryExperience.ts
@@ -16,50 +16,52 @@ export interface RegistryFilter {
 }
 
 export interface RegistryExperienceOptions extends ContextValueFilterQuickPickOptions {
-    // if registryFilter is not provided, all registries will be shown in the quick pick
+    // if registryFilter is undefined, all registries will be shown in the quick pick
     registryFilter?: RegistryFilter;
 }
 
-export async function registryExperience<TPick>(context: IActionContext, tdp: UnifiedRegistryTreeDataProvider, options: RegistryExperienceOptions): Promise<TPick> {
+export async function registryExperience(context: IActionContext, tdp: UnifiedRegistryTreeDataProvider, options: RegistryExperienceOptions): Promise<UnifiedRegistryItem<RegistryItem>> {
+    // get the registry provider unified item
     const registryProviderStep: AzureWizardPromptStep<QuickPickWizardContext>[] = [
         new RegistryQuickPickStep(
             tdp,
             options
         )
     ];
-
     const unifiedRegistryItem = await runQuickPickWizard(context, {
         hideStepCount: true,
         promptSteps: registryProviderStep,
     });
 
+    // if no registries are found, throw an error
     if (!unifiedRegistryItem) {
         context.errorHandling.suppressReportIssue = true;
         throw new Error(l10n.t('No registries found'));
     }
 
+    // actually get the registry item from the provider
     const promptSteps: AzureWizardPromptStep<QuickPickWizardContext>[] = [
         new RecursiveQuickPickStep(
             (unifiedRegistryItem as UnifiedRegistryItem<RegistryItem>).provider,
             {
-                contextValueFilter: options.contextValueFilter,
+                ...options,
                 skipIfOne: true,
             }
         )
     ];
-
     const pickWithoutProvider = await runQuickPickWizard(context, {
         hideStepCount: true,
         promptSteps: promptSteps,
     });
 
+    // add the provider back to the registry item
     const pickWithProvider: UnifiedRegistryItem<RegistryItem> = {
         wrappedItem: pickWithoutProvider,
         provider: (unifiedRegistryItem as UnifiedRegistryItem<RegistryItem>).provider,
         parent: undefined, // TODO: figure out how to get the parent
     };
 
-    return pickWithProvider as unknown as TPick;
+    return pickWithProvider;
 }
 
 export class RegistryQuickPickStep extends GenericQuickPickStep<QuickPickWizardContext, RegistryExperienceOptions> {
@@ -81,6 +83,7 @@ export class RegistryQuickPickStep extends GenericQuickPickStep<QuickPickWizardC
             });
         }
 
+        // logic copied from GenericQuickPickStep
         const childItems = await Promise.all(childElements.map(async childElement => await this.treeDataProvider.getTreeItem(childElement)));
         const childPairs: [UnifiedRegistryItem<CommonRegistryRoot>, TreeItem][] = childElements.map((childElement, i: number) => [childElement, childItems[i]]);
 
@@ -92,7 +95,7 @@ export class RegistryQuickPickStep extends GenericQuickPickStep<QuickPickWizardC
         return picks;
     }
 
-    // copied from GenericQuickPickStep
+    // clogic opied from GenericQuickPickStep
     protected async getQuickPickItem(element: UnifiedRegistryItem<CommonRegistryRoot>, item: TreeItem): Promise<IAzureQuickPickItem<UnifiedRegistryItem<CommonRegistryRoot>>> {
         return {
             label: ((item.label as TreeItemLabel)?.label || item.label) as string,
