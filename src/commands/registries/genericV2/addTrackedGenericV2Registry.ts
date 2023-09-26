610663278ee5db628bbbaadcd13223df1504f9b4
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-09-26 10:47:39 -0400

    fix genericV2 root not showing up when adding registry

--- src/commands/registries/genericV2/addTrackedGenericV2Registry.ts
+++ src/commands/registries/genericV2/addTrackedGenericV2Registry.ts
@@ -8,7 +8,17 @@ import { ext } from "../../../extensionVariables";
 import { UnifiedRegistryItem } from "../../../tree/registries/UnifiedRegistryTreeDataProvider";
 
 export async function addTrackedGenericV2Registry(context: IActionContext, node?: UnifiedRegistryItem<unknown>): Promise<void> {
-    await ext.genericRegistryV2DataProvider.addTrackedRegistry();
+    const root = ext.genericRegistryV2DataProvider.getRoot();
+    const registries = await ext.genericRegistryV2DataProvider.getRegistries(root);
+
+    if (registries?.length > 0) {
+        // if there are already registries, add a new registry to the existing root node
+        await ext.genericRegistryV2DataProvider.addTrackedRegistry();
+    } else {
+        // if there are no registries, connect as usual
+        await ext.registriesTree.connectRegistryProvider(ext.genericRegistryV2DataProvider);
+    }
+
     // don't wait
     void ext.registriesTree.refresh();
 }
