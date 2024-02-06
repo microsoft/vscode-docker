907afec8c88a35554d21067169c315e8e85cdce2
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2024-02-06 10:01:11 -0500

    Partially revert "Go back to using `python` instead of `python3` and bump Python dependency versions (#4223)"
    This partially reverts commit 7b57512a3f57419880c401fceef4cf35e71038ab.

--- src/tasks/python/PythonTaskHelper.ts
+++ src/tasks/python/PythonTaskHelper.ts
@@ -117,8 +117,8 @@ export class PythonTaskHelper implements TaskHelper {
         // User input is honored in all of the below.
         runOptions.volumes = this.inferVolumes(runOptions, launcherFolder);
 
-        // If the user specifies command, we won't set entrypoint; otherwise if they set entrypoint we will respect it; otherwise use 'python' to start an idle container
-        runOptions.entrypoint = runOptions.command ? undefined : runOptions.entrypoint || 'python';
+        // If the user specifies command, we won't set entrypoint; otherwise if they set entrypoint we will respect it; otherwise use 'python3' to start an idle container
+        runOptions.entrypoint = runOptions.command ? undefined : runOptions.entrypoint || 'python3';
 
         return runOptions;
     }
