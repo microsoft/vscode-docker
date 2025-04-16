51395b21fdc53a60bb756256f42719cbec008692
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-04-16 15:23:49 -0400

    Add a "Remind Me" button

--- src/utils/migrateToContainerTools.ts
+++ src/utils/migrateToContainerTools.ts
@@ -9,6 +9,9 @@ import { ext } from '../extensionVariables';
 
 const dontShowAgainKey = 'vscode-docker.migrateToContainerTools.dontShowAgain';
 
+const reminderIntervalMs = 60 * 60 * 60 * 1000; // 60 hours (2.5 days) in milliseconds
+const lastShownKey = 'vscode-docker.migrateToContainerTools.lastShown';
+
 export async function migrateToContainerTools(): Promise<void> {
     const shouldInstall = await callWithTelemetryAndErrorHandling<boolean>('vscode-docker.migrateToContainerTools', async (context: IActionContext) => {
         context.telemetry.properties.isActivationEvent = 'true';
@@ -21,19 +24,33 @@ export async function migrateToContainerTools(): Promise<void> {
             return false;
         }
 
+        // Check if the user has already seen the notification within the reminder interval
+        const lastShown = ext.context.globalState.get<number>(lastShownKey, 0);
+        const now = Date.now();
+        if (lastShown && now - lastShown < reminderIntervalMs) {
+            context.telemetry.suppressAll = true;
+            return false;
+        }
+
+        // Update the last shown time to now
+        await ext.context.globalState.update(lastShownKey, now);
+
         const message = vscode.l10n.t('The Docker extension is becoming the Container Tools extension. You can try Container Tools now. If you install it now, the Docker extension will be uninstalled, and the window reloaded. Make sure to save your changes before proceeding. [Learn More](https://aka.ms/vscode-container-tools-learn-more)');
 
-        const tryNowButton = vscode.l10n.t('Install and Reload Now');
+        const tryNowButton = vscode.l10n.t('Install and Reload');
+        const remindMeButton = vscode.l10n.t('Remind Me');
         const dontShowAgainButton = vscode.l10n.t('Don\'t Show Again');
 
-        const result = await vscode.window.showInformationMessage(message, tryNowButton, dontShowAgainButton);
+        // Show the notification
+        const result = await vscode.window.showInformationMessage(message, tryNowButton, remindMeButton, dontShowAgainButton);
 
         if (result === tryNowButton) {
-            // User opted in to install the Container Tools extension
             context.telemetry.properties.choice = 'install';
             return true;
+        } else if (result === remindMeButton) {
+            context.telemetry.properties.choice = 'remindMe';
+            return false;
         } else if (result === dontShowAgainButton) {
-            // User opted out of the notification
             context.telemetry.properties.choice = 'dontShowAgain';
             await ext.context.globalState.update(dontShowAgainKey, true);
             return false;
